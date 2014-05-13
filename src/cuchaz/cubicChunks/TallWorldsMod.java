fadf5d049bb0666c74569b16796ba86a745275c2
jeff <none@none>
2014-05-13 12:58:48 -0400

    moved rendering code from base edits to event handlers

--- src/cuchaz/cubicChunks/TallWorldsMod.java
+++ src/cuchaz/cubicChunks/TallWorldsMod.java
@@ -12,6 +12,7 @@ package cuchaz.cubicChunks;
 
 import java.io.IOException;
 
+import net.minecraft.client.renderer.WorldRenderer;
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.world.WorldServer;
 
@@ -31,6 +32,7 @@ import cuchaz.magicMojoModLoader.api.events.BuildHeightEvent;
 import cuchaz.magicMojoModLoader.api.events.ClassOverrideEvent;
 import cuchaz.magicMojoModLoader.api.events.EncodeChunkEvent;
 import cuchaz.magicMojoModLoader.api.events.EntityPlayerMPUpdateEvent;
+import cuchaz.magicMojoModLoader.api.events.UpdateRenderPositionEvent;
 
 public class TallWorldsMod implements Mod
 {
@@ -97,5 +99,130 @@ public class TallWorldsMod implements Mod
 		WorldServer world = (WorldServer)player.theItemInWorldManager.theWorld;
 		CubePlayerManager playerManager = (CubePlayerManager)world.getPlayerManager();
 		playerManager.onPlayerUpdate( player );
+	}
+	
+	public void handleEvent( UpdateRenderPositionEvent event )
+	{
+		// block position is the position of the viewpoint entity
+		int blockX = event.getBlockX();
+		int blockY = event.getBlockY();
+		int blockZ = event.getBlockZ();
+		
+		// move back 8 blocks?? (why?)
+		blockX -= 8;
+		blockY -= 8;
+		blockZ -= 8;
+		
+		int minBlockX = Integer.MAX_VALUE;
+		int minBlockY = Integer.MAX_VALUE;
+		int minBlockZ = Integer.MAX_VALUE;
+		int maxBlockX = Integer.MIN_VALUE;
+		int maxBlockY = Integer.MIN_VALUE;
+		int maxBlockZ = Integer.MIN_VALUE;
+		
+		// get view dimensions
+		int blockViewDx = event.getRenderCubeDx()*16;
+		int blockViewHdx = blockViewDx/2;
+		int blockViewDy = event.getRenderCubeDy()*16;
+		int blockViewHdy = blockViewDy/2;
+		int blockViewDz = event.getRenderCubeDz()*16;
+		int blockViewHdz = blockViewDz/2;
+		
+		for( int renderX=0; renderX<event.getRenderCubeDx(); renderX++ )
+		{
+			int posBlockX = renderX*16;
+			
+			// compute parameter of coordinate transformation
+			int blockWidthsX = posBlockX + blockViewHdx - blockX;
+			if( blockWidthsX < 0 )
+			{
+				blockWidthsX -= blockViewDx - 1;
+			}
+			blockWidthsX /= blockViewDx;
+			
+			// translate by player position
+			posBlockX -= blockWidthsX*blockViewDx;
+			
+			// update bounds
+			if( posBlockX < minBlockX )
+			{
+				minBlockX = posBlockX;
+			}
+			if( posBlockX > maxBlockX )
+			{
+				maxBlockX = posBlockX;
+			}
+			
+			for( int renderZ=0; renderZ<event.getRenderCubeDz(); renderZ++ )
+			{
+				int posBlockZ = renderZ*16;
+				
+				// compute parameter of coordinate transformation
+				int blockWidthsZ = posBlockZ + blockViewHdz - blockZ;
+				if( blockWidthsZ < 0 )
+				{
+					blockWidthsZ -= blockViewDz - 1;
+				}
+				blockWidthsZ /= blockViewDz;
+				
+				// translate by player position
+				posBlockZ -= blockWidthsZ*blockViewDz;
+				
+				// update bounds
+				if( posBlockZ < minBlockZ )
+				{
+					minBlockZ = posBlockZ;
+				}
+				if( posBlockZ > maxBlockZ )
+				{
+					maxBlockZ = posBlockZ;
+				}
+				
+				for( int renderY=0; renderY<event.getRenderCubeDy(); renderY++ )
+				{
+					int posBlockY = renderY*16;
+					
+					// compute parameter of coordinate transformation
+					int blockHeightsY = posBlockY + blockViewHdy - blockY;
+					if( blockHeightsY < 0 )
+					{
+						blockHeightsY -= blockViewDy - 1;
+					}
+					blockHeightsY /= blockViewDy;
+					
+					// translate by player position
+					posBlockY -= blockHeightsY*blockViewDy;
+					
+					// update bounds
+					if( posBlockY < minBlockY )
+					{
+						minBlockY = posBlockY;
+					}
+					if( posBlockY > maxBlockY )
+					{
+						maxBlockY = posBlockY;
+					}
+					
+					// update renderer
+					WorldRenderer renderer = event.getRenderer( renderX, renderY, renderZ );
+					boolean neededUpdate = renderer.needsUpdate;
+					renderer.setPosition( posBlockX, posBlockY, posBlockZ );
+					if( !neededUpdate && renderer.needsUpdate )
+					{
+						event.updateRenderer( renderer );
+					}
+				}
+			}
+		}
+		
+		// save the bounds to the event
+		event.setMinBlockX( minBlockX );
+		event.setMinBlockY( minBlockY );
+		event.setMinBlockZ( minBlockZ );
+		event.setMaxBlockX( maxBlockX );
+		event.setMaxBlockY( maxBlockY );
+		event.setMaxBlockZ( maxBlockZ );
+		
+		event.setHandled();
 	}
 }
