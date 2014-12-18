d809737d7fdbdec384c9dcf808c60268884fb158
Barteks2x <none@none>
2014-12-18 12:08:59 +0100

    Removed unnecessary constants from GlobalGeneratorConfig
    --HG--
    branch : new-biomesystem-experimental

--- src/cuchaz/cubicChunks/generator/terrain/GlobalGeneratorConfig.java
+++ src/cuchaz/cubicChunks/generator/terrain/GlobalGeneratorConfig.java
@@ -1,5 +1,6 @@
 package cuchaz.cubicChunks.generator.terrain;
 
+import cuchaz.cubicChunks.util.Coords;
 import static cuchaz.cubicChunks.util.Coords.CUBE_MAX_X;
 import static cuchaz.cubicChunks.util.Coords.CUBE_MAX_Y;
 import static cuchaz.cubicChunks.util.Coords.CUBE_MAX_Z;
@@ -13,15 +14,7 @@ public class GlobalGeneratorConfig
 	public static final int Y_SECTION_SIZE = 8 + 1;
 	public static final int Z_SECTION_SIZE = 4 + 1;
 	
-	public static final int CUBE_X_SIZE = 16;
-	
-	public static final int CUBE_Y_SIZE = 16;
-	
-	public static final int CUBE_Z_SIZE = 16;
-	
-	public static final int X_SECTIONS = CUBE_X_SIZE / ( X_SECTION_SIZE - 1 ) + 1;
-	
-	public static final int Y_SECTIONS = CUBE_Y_SIZE / ( Y_SECTION_SIZE - 1 ) + 1;
-	
-	public static final int Z_SECTIONS = CUBE_Z_SIZE / ( Z_SECTION_SIZE - 1 ) + 1;
+	public static final int X_SECTIONS = Coords.CUBE_MAX_X / ( X_SECTION_SIZE - 1 ) + 1;
+	public static final int Y_SECTIONS = Coords.CUBE_MAX_Y / ( Y_SECTION_SIZE - 1 ) + 1;
+	public static final int Z_SECTIONS = Coords.CUBE_MAX_Z / ( Z_SECTION_SIZE - 1 ) + 1;
 }
