efd417b48f0919a304465ce3ca255b1f060f276a
Cuchaz <cuchaz@gmail.com>
2015-04-05 20:17:09 -0400

    fix stupid comment that the source formatter broke

--- src/main/cubicchunks/util/AddressTools.java
+++ src/main/cubicchunks/util/AddressTools.java
@@ -27,24 +27,20 @@ public class AddressTools {
 	// Anvil format details:
 	// within a region file, each chunk coord gets 5 bits
 	// the coord for each region is capped at 27 bits
-
-	// World.setBlock() caps block x,z at -30m to 30m, so our 22-bit cap on
-	// chunk x,z is just right!
-
+	
 	// here's the encoding scheme for 64 bits of space:
-	// y: 20 bits, signed, 1,048,576 chunks, 16,777,216 blocks
-	// x: 22 bits, signed, 4,194,304 chunks, 67,108,864 blocks
-	// z: 22 bits, signed, 4,194,304 chunks, 67,108,864 blocks
-
-	// the Anvil format gives 32 bits to each chunk coordinate, but we're only
-	// giving 22 bits
-	// moving at 8 blocks/s (which is like minecart speed), it would take ~48
-	// days to reach the x or z edge from the center
-	// at the same speed, it would take ~24 days to reach the top from the
-	// bottom
+	// y:         20 bits, signed,   1,048,576 chunks, 16,777,216 blocks
+	// x:         22 bits, signed,   4,194,304 chunks, 67,108,864 blocks
+	// z:         22 bits, signed,   4,194,304 chunks, 67,108,864 blocks
+	
+	// World.setBlock() caps block x,z at -30m to 30m, so our 22-bit cap on chunk x,z is just right!
+	
+	// the Anvil format gives 32 bits to each chunk coordinate, but we're only giving 22 bits
+	// moving at 8 blocks/s (which is like minecart speed), it would take ~48 days to reach the x or z edge from the center
+	// at the same speed, it would take ~24 days to reach the top from the bottom
 	// that seems like enough room for a minecraft world
-
-	// 0 1 2 3| 4 | 5 6 |
+	
+	// 0         1        |2         3         4|        5         6  |
 	// 0123456789012345678901234567890123456789012345678901234567890123
 	// yyyyyyyyyyyyyyyyyyyyxxxxxxxxxxxxxxxxxxxxxxzzzzzzzzzzzzzzzzzzzzzz
 
