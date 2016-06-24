3d8b1fac9ab9a67ef6c3a268886c2fdcfe4e3db8
Bartosz Skrzypczak <Bartek_s2@wp.pl>
2016-06-24 03:01:33 +0200

    Update mixins to 1.10, fix previously uncommitted version chnages.

--- src/main/java/cubicchunks/asm/mixin/noncritical/common/command/MixinCommandTP.java
+++ src/main/java/cubicchunks/asm/mixin/noncritical/common/command/MixinCommandTP.java
@@ -24,9 +24,10 @@
 package cubicchunks.asm.mixin.noncritical.common.command;
 
 import cubicchunks.world.ICubicWorld;
+import net.minecraft.command.CommandTP;
 import net.minecraft.command.EntityNotFoundException;
 import net.minecraft.command.ICommandSender;
-import net.minecraft.command.server.CommandTeleport;
+import net.minecraft.command.PlayerNotFoundException;
 import net.minecraft.entity.Entity;
 import net.minecraft.server.MinecraftServer;
 import org.spongepowered.asm.mixin.Mixin;
@@ -36,21 +37,34 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import static cubicchunks.asm.JvmNames.COMMAND_TELEPORT_GET_ENTITY;
+import static cubicchunks.asm.JvmNames.COMMAND_TP_GET_COMMAND_SENDER_AS_PLAYER;
+import static cubicchunks.asm.JvmNames.COMMAND_TP_GET_ENTITY;
+import static net.minecraft.command.CommandBase.getCommandSenderAsPlayer;
 import static net.minecraft.command.CommandBase.getEntity;
 
-@Mixin(CommandTeleport.class)
-public class MixinCommandTeleport {
+@Mixin(CommandTP.class)
+public class MixinCommandTP {
 	private Entity entity;
 
 	@Inject(method = "execute",
-	        at = @At(value = "INVOKE", target = COMMAND_TELEPORT_GET_ENTITY, ordinal = 0),
+	        at = @At(value = "INVOKE", target = COMMAND_TP_GET_ENTITY, ordinal = 0),
 	        require = 1)
 	private void postGetEntityInject(MinecraftServer server, ICommandSender sender, String args[], CallbackInfo ci) {
 		try {
 			entity = getEntity(server, sender, args[0]);
 		} catch (EntityNotFoundException e) {
-			e.printStackTrace();
+		}
+	}
+
+	@Inject(
+			method = "execute",
+			at = @At(value = "INVOKE", target = COMMAND_TP_GET_COMMAND_SENDER_AS_PLAYER, ordinal = 0),
+			require = 1
+	)
+	private void postGetEntityPlayerInject(MinecraftServer server, ICommandSender sender, String args[], CallbackInfo ci) {
+		try {
+			entity = getCommandSenderAsPlayer(sender);
+		} catch (PlayerNotFoundException e) {
 		}
 	}
 
@@ -59,15 +73,14 @@ public class MixinCommandTeleport {
 		if (entity == null) {
 			return orig;
 		}
-		System.out.println(((ICubicWorld) entity.getEntityWorld()).getMinHeight());
 		return ((ICubicWorld) entity.getEntityWorld()).getMinHeight() + orig;
 	}
 
-	@ModifyConstant(method = "execute", constant = @Constant(intValue = 4096), require = 2)
+	@ModifyConstant(method = "execute", constant = @Constant(intValue = 4096), require = 1)
 	private int getMaxY(int orig) {
 		if (entity == null) {
 			return orig;
 		}
-		return ((ICubicWorld) entity.getEntityWorld()).getMaxHeight() + orig - 256;
+		return ((ICubicWorld) entity.getEntityWorld()).getMaxHeight() + orig;
 	}
 }
