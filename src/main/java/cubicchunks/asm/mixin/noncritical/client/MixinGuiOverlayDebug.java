140042bca7543919dd97a11f7c36769ffb5b1ca3
Malte Schütze <malte.schuetze@fgms.de>
2016-09-27 15:24:45 +0200

    GuiDebugOverlay: Use MixinUtils

--- src/main/java/cubicchunks/asm/mixin/noncritical/client/MixinGuiOverlayDebug.java
+++ src/main/java/cubicchunks/asm/mixin/noncritical/client/MixinGuiOverlayDebug.java
@@ -23,17 +23,18 @@
  */
 package cubicchunks.asm.mixin.noncritical.client;
 
-import cubicchunks.world.ICubicWorldClient;
+import cubicchunks.asm.MixinUtils;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiOverlayDebug;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.MathHelper;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
-import static cubicchunks.asm.JvmNames.GUI_OVERLAY_DEBUG_CALL;
+
 import static cubicchunks.asm.JvmNames.BLOCK_POS_GETY;
+import static cubicchunks.asm.JvmNames.GUI_OVERLAY_DEBUG_CALL;
 
 /**
  * Goal of this mixin is to remove the "Outside of world..." message on the debug overlay for cubic world types.
@@ -42,31 +43,19 @@ import static cubicchunks.asm.JvmNames.BLOCK_POS_GETY;
  */
 @Mixin(GuiOverlayDebug.class)
 public class MixinGuiOverlayDebug {
-	@Shadow private Minecraft mc;
+	@Shadow @Final private Minecraft mc;
 
 	// As of Minecraft 1.10.2 / Forge 12.18.1.2092, the bounds checks are call 5 and 6 to getY()
 
 	// This is call 5 (idx 4)
 	@Redirect(method = GUI_OVERLAY_DEBUG_CALL, at = @At(value = "INVOKE", target = BLOCK_POS_GETY, ordinal = 4))
 	private int blockPosGetYBoundsCheck1(BlockPos pos) {
-		return blockPosGetYRedirect(pos);
+		return MixinUtils.getReplacementY(mc.theWorld, pos);
 
 	}
 	// This is call 6 (idx 5)
 	@Redirect(method = GUI_OVERLAY_DEBUG_CALL, at = @At(value = "INVOKE", target = BLOCK_POS_GETY, ordinal = 5))
 	private int blockPosGetYBoundsCheck2(BlockPos pos) {
-		return blockPosGetYRedirect(pos);
-
-	}
-
-	private int blockPosGetYRedirect(BlockPos pos) {// In bounds - don't modify anything
-		if (pos.getY() >= 0 && pos.getY() < 256) return pos.getY();
-
-		// Retain normal semantics for non-cubic worlds
-		if (mc.theWorld instanceof ICubicWorldClient) {
-			return MathHelper.clamp_int(pos.getY(), 0, 255);
-		}
-
-		return pos.getY();
+		return MixinUtils.getReplacementY(mc.theWorld, pos);
 	}
 }
