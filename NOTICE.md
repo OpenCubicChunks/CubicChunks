56c72c354047e6887151b5ea3265174ba25d47e9
Malte Schütze <malte.schuetze@fgms.de>
2016-10-16 02:57:57 +0200

    Licensing foo

---
+++ NOTICE.md
@@ -0,0 +1,7 @@
+# Credits
+
+The asynchronous chunk loading executor code in `cubicchunks.server.chunkio.async` is based on parts of
+[MinecraftForge](https://github.com/MinecraftForge/MinecraftForge) and licensed under the
+[GNU Lesser General Public License v2.1](http://www.gnu.org/licenses/lgpl-2.1.html):
+  - [Original license text](https://github.com/MinecraftForge/MinecraftForge/blob/279380b4f1aa02a4df5e2b323c11ca6ab1531431/LICENSE-new.txt)
+  - [Original sources](https://github.com/MinecraftForge/MinecraftForge/tree/279380b4f1aa02a4df5e2b323c11ca6ab1531431/src/main/java/net/minecraftforge/common/chunkio)
--- src/main/java/cubicchunks/server/chunkio/async/AsyncIOProvider.java
+++ src/main/java/cubicchunks/server/chunkio/async/AsyncIOProvider.java
@@ -1,3 +1,22 @@
+/*
+ * Minecraft Forge
+ * Copyright (c) 2016.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation version 2.1
+ * of the License.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
 package cubicchunks.server.chunkio.async;
 
 import javax.annotation.Nullable;
@@ -7,7 +26,6 @@ import java.util.function.Consumer;
 /**
  * Interface for grouping asynchronous world IO access together, synchronized to the start of the next tick
  * after loading finishes
- * @author Malte Schütze
  */
 abstract class AsyncIOProvider<T> implements Runnable {
 	private final ConcurrentLinkedQueue<Consumer<T>> callbacks = new ConcurrentLinkedQueue<>();
