33dc40e6b29383675da8ec46af883d0fd622fe6f
Barteks2x <Barteks2x@gmail.com>
2016-11-27 08:01:08 +0100

    Add missing license header to mixin/fixes/common/MixinWorld

--- src/main/java/cubicchunks/asm/mixin/fixes/common/MixinWorld.java
+++ src/main/java/cubicchunks/asm/mixin/fixes/common/MixinWorld.java
@@ -1,3 +1,26 @@
+/*
+ *  This file is part of Cubic Chunks Mod, licensed under the MIT License (MIT).
+ *
+ *  Copyright (c) 2015 contributors
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in
+ *  all copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *  THE SOFTWARE.
+ */
 package cubicchunks.asm.mixin.fixes.common;
 
 import net.minecraft.block.state.IBlockState;
@@ -21,7 +44,7 @@ import static cubicchunks.asm.JvmNames.CHUNK_IS_POPULATED;
 public class MixinWorld {
 
 	// note: markAndNotifyBlock has @Nullable on chunk, this will never be null here,
-	// because this is the chunk on which isPopulated is called
+	// because this isgit lo the chunk on which isPopulated is called
 	@Redirect(method = "markAndNotifyBlock", at = @At(value = "INVOKE", target = CHUNK_IS_POPULATED))
 	public boolean markNotifyBlock_CubeCheck(Chunk _this,
 	                                         BlockPos pos, Chunk chunk, IBlockState oldstate,
