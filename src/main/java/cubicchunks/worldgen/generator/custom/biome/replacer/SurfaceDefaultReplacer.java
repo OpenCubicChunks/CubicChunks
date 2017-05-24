4652c8e1260bcc85fafeaa7f7ac0c06ae0eed967
Barteks2x <Barteks2x@gmail.com>
2017-05-24 19:56:22 +0200

    Improved default surface block replacer
    Gravel at the bottom of the ocean, sandstone in the desert

--- src/main/java/cubicchunks/worldgen/generator/custom/biome/replacer/SurfaceDefaultReplacer.java
+++ src/main/java/cubicchunks/worldgen/generator/custom/biome/replacer/SurfaceDefaultReplacer.java
@@ -27,7 +27,11 @@ import com.google.common.collect.Sets;
 import cubicchunks.CubicChunks;
 import cubicchunks.world.ICubicWorld;
 import cubicchunks.api.worldgen.biome.CubicBiome;
+import cubicchunks.worldgen.generator.custom.ConversionUtils;
+import cubicchunks.worldgen.generator.custom.builder.IBuilder;
+import cubicchunks.worldgen.generator.custom.builder.NoiseSource;
 import mcp.MethodsReturnNonnullByDefault;
+import net.minecraft.block.BlockSand;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.init.Blocks;
 import net.minecraft.util.ResourceLocation;
@@ -40,20 +44,31 @@ import javax.annotation.ParametersAreNonnullByDefault;
 @ParametersAreNonnullByDefault
 @MethodsReturnNonnullByDefault
 public class SurfaceDefaultReplacer implements IBiomeBlockReplacer {
+    protected static final IBlockState GRAVEL = Blocks.GRAVEL.getDefaultState();
+    protected static final IBlockState RED_SANDSTONE = Blocks.RED_SANDSTONE.getDefaultState();
+    protected static final IBlockState SANDSTONE = Blocks.SANDSTONE.getDefaultState();
 
+    private final IBuilder depthNoise;
+    private final int maxPossibleDepth;
     private IBlockState topBlock;
     private IBlockState fillerBlock;
-    private final double horizontalGradientWeight;
-    private final double depth;
+    private final double horizontalGradientCutoffWeight;
+    private final double horizontalGradientDepthDecreaseWeight;
     private final double oceanHeight;
 
-    public SurfaceDefaultReplacer(IBlockState topBlock, IBlockState fillerBlock, double horizontalGradientWeight, double depthMultiplier,
-            double oceanHeight) {
+    public SurfaceDefaultReplacer(IBlockState topBlock, IBlockState fillerBlock,
+            double horizontalGradientCutoffWeight, double horizontalGradientDepthDecreaseWeight, double oceanHeight) {
         this.topBlock = topBlock;
         this.fillerBlock = fillerBlock;
-        this.horizontalGradientWeight = horizontalGradientWeight;
-        this.depth = depthMultiplier;
+        this.horizontalGradientCutoffWeight = horizontalGradientCutoffWeight;
+        this.horizontalGradientDepthDecreaseWeight = horizontalGradientDepthDecreaseWeight;
         this.oceanHeight = oceanHeight;
+        this.depthNoise = NoiseSource.perlin()
+                .frequency(ConversionUtils.frequencyFromVanilla(0.0625f, 4)).octaves(4).create()
+                .mul(ConversionUtils.maxValueMultipler(4))
+                .mul(1.0 / 3.0).add(3)
+                .cached2d(256, v -> v.getX() + v.getZ() * 16);
+        this.maxPossibleDepth = 9;
     }
 
     /**
@@ -61,44 +76,65 @@ public class SurfaceDefaultReplacer implements IBiomeBlockReplacer {
      */
     @Override
     public IBlockState getReplacedBlock(IBlockState previousBlock, int x, int y, int z, double dx, double dy, double dz, double density) {
+        // skip everything below if there is no chance it will actually do something
+        if (density > maxPossibleDepth || density < 0) {
+            return previousBlock;
+        }
         if (previousBlock.getBlock() == Blocks.AIR) {
             return previousBlock;
         }
-        if (density + dy <= 0) {
-            if (y < oceanHeight - 1) {
-                return fillerBlock;
-            } else {
-                return topBlock;
+        double depth = depthNoise.get(x, 0, z);
+        if (density + dy <= 0) { // if air above
+            if (y < oceanHeight - 7 - depth) { // if we are deep into the ocean
+                return GRAVEL;
             }
+            if (y < oceanHeight - 1) { // if just below the ocean level
+                return filler(depth);
+            }
+            return top(previousBlock, depth);
         } else {
-            // I don't have a good idea for variable name here
-            double d = dy + horizontalGradientWeight * Math.sqrt(dx * dx + dz * dz);
-            if (d < 0 && density < depth) {
+            double xzSize = Math.sqrt(dx * dx + dz * dz);
+            double dyAdjusted = dy + horizontalGradientCutoffWeight * xzSize;
+            if (dyAdjusted < 0 && density < depth + 1 - horizontalGradientDepthDecreaseWeight * xzSize) {
                 return fillerBlock;
+            }
+
+            if (fillerBlock.getBlock() == Blocks.SAND && depth > 1 && y > oceanHeight - depth) {
+                return fillerBlock.getValue(BlockSand.VARIANT) == BlockSand.EnumType.RED_SAND ? RED_SANDSTONE : SANDSTONE;
+            } else if (depth <=1){
+                System.out.println("x=" + x + ", z=" + z);
             }
         }
         return previousBlock;
     }
 
+    private IBlockState filler(double depth) {
+        return depth > 0 ? fillerBlock : Blocks.AIR.getDefaultState();
+    }
+
+    private IBlockState top(IBlockState prev, double depth) {
+        return depth > 0 ? topBlock : prev;
+    }
+
     public static IBiomeBlockReplacerProvider provider() {
         return new IBiomeBlockReplacerProvider() {
-            private final ResourceLocation HORIZONTAL_GRADIENT_WEIGHT = CubicChunks.location("horizontal_gradient_weight");
-            private final ResourceLocation FILLER_DEPTH_MULTIPLIER = CubicChunks.location("filler_depth_multiplier");
+            private final ResourceLocation HORIZONTAL_GRADIENT_WEIGHT = CubicChunks.location("horizontal_gradient_cutoff_weight");
+            private final ResourceLocation HORIZONTAL_GRADIENT_DEC = CubicChunks.location("horizontal_gradient_depth_decrease_weight");
             private final ResourceLocation OCEAN_LEVEL = CubicChunks.location("ocean_level");
 
             @Override
             public IBiomeBlockReplacer create(ICubicWorld world, CubicBiome cubicBiome, BiomeBlockReplacerConfig conf) {
                 double gradWeight = conf.getDouble(HORIZONTAL_GRADIENT_WEIGHT);
-                double depthMult = conf.getDouble(FILLER_DEPTH_MULTIPLIER);
+                double gradientDec = conf.getDouble(HORIZONTAL_GRADIENT_DEC);
                 double oceanY = conf.getInt(OCEAN_LEVEL);
                 Biome biome = cubicBiome.getBiome();
-                return new SurfaceDefaultReplacer(biome.topBlock, biome.fillerBlock, gradWeight, depthMult, oceanY);
+                return new SurfaceDefaultReplacer(biome.topBlock, biome.fillerBlock, gradWeight, gradientDec, oceanY);
             }
 
             @Override public Set<ConfigOptionInfo> getPossibleConfigOptions() {
                 return Sets.newHashSet(
-                        new ConfigOptionInfo(HORIZONTAL_GRADIENT_WEIGHT, 0.25),
-                        new ConfigOptionInfo(FILLER_DEPTH_MULTIPLIER, 4.0)
+                        new ConfigOptionInfo(HORIZONTAL_GRADIENT_WEIGHT, -0.2),
+                        new ConfigOptionInfo(HORIZONTAL_GRADIENT_DEC, 0.2)
                 );
             }
         };
