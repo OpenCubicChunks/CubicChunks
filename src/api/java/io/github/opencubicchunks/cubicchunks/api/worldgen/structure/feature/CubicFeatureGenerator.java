7fd5bc3859216f3870fab4700f0c8192b14add01
Foghrye4 <foghrye4@gmail.com>
2019-07-28 23:14:48 +0300

    Moving some CWG classes to API

---
+++ src/api/java/io/github/opencubicchunks/cubicchunks/api/worldgen/structure/feature/CubicFeatureGenerator.java
@@ -0,0 +1,202 @@
+/*
+ *  This file is part of Cubic Chunks Mod, licensed under the MIT License (MIT).
+ *
+ *  Copyright (c) 2015-2019 OpenCubicChunks
+ *  Copyright (c) 2015-2019 contributors
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in
+ *  all copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *  THE SOFTWARE.
+ */
+package io.github.opencubicchunks.cubicchunks.api.worldgen.structure.feature;
+
+import static io.github.opencubicchunks.cubicchunks.api.util.Coords.cubeToCenterBlock;
+
+import io.github.opencubicchunks.cubicchunks.api.worldgen.CubePrimer;
+import io.github.opencubicchunks.cubicchunks.api.worldgen.CubicStructureGenerator;
+import io.github.opencubicchunks.cubicchunks.api.util.CubePos;
+import io.github.opencubicchunks.cubicchunks.api.util.XYZMap;
+import io.github.opencubicchunks.cubicchunks.api.world.ICube;
+import mcp.MethodsReturnNonnullByDefault;
+import net.minecraft.crash.CrashReport;
+import net.minecraft.crash.CrashReportCategory;
+import net.minecraft.nbt.NBTBase;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.util.ReportedException;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.minecraft.world.gen.structure.MapGenStructureIO;
+import net.minecraft.world.gen.structure.StructureBoundingBox;
+import net.minecraft.world.gen.structure.StructureComponent;
+import net.minecraft.world.gen.structure.StructureStart;
+import net.minecraftforge.common.util.Constants;
+
+import java.util.Random;
+
+import javax.annotation.Nullable;
+import javax.annotation.ParametersAreNonnullByDefault;
+
+@ParametersAreNonnullByDefault
+@MethodsReturnNonnullByDefault
+public abstract class CubicFeatureGenerator extends CubicStructureGenerator {
+
+    private CubicFeatureData structureData;
+
+    /**
+     * Used to store a list of all structures that have been recursively generated. Used so that during recursive
+     * generation, the structure generator can avoid generating structures that intersect ones that have already been
+     * placed.
+     */
+    protected XYZMap<ICubicStructureStart> structureMap = new XYZMap<>(0.5f, 1024);
+
+    /**
+     * @param spacing The minimum spacing. Structures thataren't generated at integer multiple coords of this value will be skipped.
+     */
+    protected CubicFeatureGenerator(int spacing) {
+        super(spacing);
+    }
+
+    public abstract String getStructureName();
+
+    @SuppressWarnings("ConstantConditions")
+    @Override public void generate(World world, @Nullable CubePrimer cube, CubePos cubePos) {
+        super.generate(world, cube, cubePos);
+    }
+
+    @Override
+    protected synchronized void generate(World world, @Nullable CubePrimer cube, int structureX, int structureY, int structureZ,
+            CubePos generatedCubePos) {
+        this.initializeStructureData((World) world);
+
+        if (!this.structureMap.contains(structureX, structureY, structureZ)) {
+            this.rand.nextInt();
+            try {
+                if (this.canSpawnStructureAtCoords(structureX, structureY, structureZ)) {
+                    StructureStart start = this.getStructureStart(structureX, structureY, structureZ);
+                    this.structureMap.put((ICubicStructureStart) start);
+                    if (start.isSizeableStructure()) {
+                        this.setStructureStart(structureX, structureY, structureZ, start);
+                    }
+                }
+            } catch (Throwable throwable) {
+                CrashReport report = CrashReport.makeCrashReport(throwable, "Exception preparing structure feature");
+                CrashReportCategory category = report.makeCategory("Feature being prepared");
+                category.addDetail("Is feature chunk", () -> this.canSpawnStructureAtCoords(structureX, structureY, structureZ) ? "True" : "False");
+                category.addCrashSection("Chunk location", String.format("%d,%d,%d", structureX, structureY, structureZ));
+                category.addDetail("Structure type", () -> this.getClass().getCanonicalName());
+                throw new ReportedException(report);
+            }
+        }
+    }
+
+    public synchronized boolean generateStructure(World world, Random rand, CubePos cubePos) {
+        this.initializeStructureData(world);
+        int centerX = cubeToCenterBlock(cubePos.getX());
+        int centerY = cubeToCenterBlock(cubePos.getY());
+        int centerZ = cubeToCenterBlock(cubePos.getZ());
+        boolean generated = false;
+        for (ICubicStructureStart cubicStructureStart : this.structureMap) {
+            StructureStart structStart = (StructureStart) cubicStructureStart;
+            // TODO: cubic chunks version of isValidForPostProcess and notifyPostProcess (mixin)
+            if (structStart.isSizeableStructure() && structStart.isValidForPostProcess(cubePos.chunkPos())
+                    && structStart.getBoundingBox().intersectsWith(
+                    new StructureBoundingBox(centerX, centerY, centerZ, centerX + ICube.SIZE - 1, centerY + ICube.SIZE - 1, centerZ + ICube.SIZE - 1))) {
+                structStart.generateStructure(world, rand,
+                        new StructureBoundingBox(centerX, centerY, centerZ, centerX + ICube.SIZE - 1, centerY + ICube.SIZE - 1, centerZ + ICube.SIZE - 1));
+                structStart.notifyPostProcessAt(cubePos.chunkPos());
+                generated = true;
+                this.setStructureStart(structStart.getChunkPosX(), cubicStructureStart.getChunkPosY(), structStart.getChunkPosZ(), structStart);
+            }
+        }
+
+        return generated;
+    }
+
+    public boolean isInsideStructure(BlockPos pos) {
+        this.initializeStructureData((World) this.world);
+        return this.getStructureAt(pos) != null;
+    }
+
+    @Nullable
+    protected StructureStart getStructureAt(BlockPos pos) {
+        for (ICubicStructureStart cubicStructureStart : this.structureMap) {
+            StructureStart start = (StructureStart) cubicStructureStart;
+
+            if (start.isSizeableStructure() && start.getBoundingBox().isVecInside(pos)) {
+                for (StructureComponent component : start.getComponents()) {
+                    if (component.getBoundingBox().isVecInside(pos)) {
+                        return start;
+                    }
+                }
+            }
+        }
+        return null;
+    }
+
+    public boolean isPositionInStructure(World world, BlockPos pos) {
+        this.initializeStructureData(world);
+        for (ICubicStructureStart cubicStart : this.structureMap) {
+            StructureStart start = (StructureStart) cubicStart;
+            if (start.isSizeableStructure() && start.getBoundingBox().isVecInside(pos)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Nullable
+    public abstract BlockPos getNearestStructurePos(World worldIn, BlockPos pos, boolean findUnexplored);
+
+    protected void initializeStructureData(World world) {
+        if (this.structureData != null) {
+            return;
+        }
+        this.structureData = (CubicFeatureData) world.getPerWorldStorage().getOrLoadData(CubicFeatureData.class, this.getStructureName());
+
+        if (this.structureData == null) {
+            this.structureData = new CubicFeatureData(this.getStructureName());
+            world.getPerWorldStorage().setData(this.getStructureName(), this.structureData);
+        } else {
+            NBTTagCompound nbttagcompound = this.structureData.getTagCompound();
+
+            for (String s : nbttagcompound.getKeySet()) {
+                NBTBase nbtbase = nbttagcompound.getTag(s);
+
+                if (nbtbase.getId() == Constants.NBT.TAG_COMPOUND) {
+                    NBTTagCompound tag = (NBTTagCompound) nbtbase;
+
+                    if (tag.hasKey("ChunkX") && tag.hasKey("ChunkY") && tag.hasKey("ChunkZ")) {
+                        StructureStart structurestart = MapGenStructureIO.getStructureStart(tag, world);
+
+                        if (structurestart != null) {
+                            this.structureMap.put((ICubicStructureStart) structurestart);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private void setStructureStart(int chunkX, int chunkY, int chunkZ, StructureStart start) {
+        this.structureData.writeInstance(start.writeStructureComponentsToNBT(chunkX, chunkZ), chunkX, chunkY, chunkZ);
+        this.structureData.markDirty();
+    }
+
+    protected abstract boolean canSpawnStructureAtCoords(int chunkX, int chunkY, int chunkZ);
+
+    protected abstract StructureStart getStructureStart(int chunkX, int chunkY, int chunkZ);
+}
---
+++ src/main/java/io/github/opencubicchunks/cubicchunks/core/asm/mixin/core/common/MixinStructureStart.java
@@ -0,0 +1,157 @@
+package io.github.opencubicchunks.cubicchunks.core.asm.mixin.core.common;
+
+/*
+ *  This file is part of Cubic Chunks Mod, licensed under the MIT License (MIT).
+ *
+ *  Copyright (c) 2015-2019 OpenCubicChunks
+ *  Copyright (c) 2015-2019 contributors
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in
+ *  all copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *  THE SOFTWARE.
+ */
+
+import static io.github.opencubicchunks.cubicchunks.api.util.Coords.localToBlock;
+
+import com.google.common.base.Preconditions;
+import io.github.opencubicchunks.cubicchunks.api.util.CubePos;
+import io.github.opencubicchunks.cubicchunks.api.world.ICubicWorld;
+import io.github.opencubicchunks.cubicchunks.api.worldgen.structure.feature.ICubicStructureStart;
+import io.github.opencubicchunks.cubicchunks.api.world.ICube;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.world.World;
+import net.minecraft.world.gen.structure.StructureBoundingBox;
+import net.minecraft.world.gen.structure.StructureComponent;
+import net.minecraft.world.gen.structure.StructureStart;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
+
+import java.util.List;
+import java.util.Random;
+
+import javax.annotation.Nullable;
+
+@Mixin(StructureStart.class)
+public abstract class MixinStructureStart implements ICubicStructureStart {
+
+    @Shadow public abstract int getChunkPosX();
+
+    @Shadow public abstract int getChunkPosZ();
+
+    @Shadow protected StructureBoundingBox boundingBox;
+    @Shadow protected List<StructureComponent> components;
+
+    private int cubeY;
+    private int expectedBaseHeight;
+    private boolean isCubic = false;
+
+    // for storing arguments of methods that need to be redone on cubic init
+    private boolean markAvailableHeightDone = false;
+    private int minDepth;
+    private int randY;
+
+    @Override public int getChunkPosY() {
+        return this.cubeY;
+    }
+
+    @Override public void initCubic(World world, int expectedBaseHeightIn, int cubeY) {
+        if (this.isCubic) {
+            throw new IllegalStateException("Already initialized!");
+        }
+        this.cubeY = cubeY;
+        this.isCubic = true;
+        this.expectedBaseHeight = expectedBaseHeightIn;
+        if (markAvailableHeightDone) {
+            markAvailableHeightCubic(world, null, minDepth, null);
+        }
+    }
+
+    @Override public CubePos getCubePos() {
+        return new CubePos(getX(), getY(), getZ());
+    }
+
+    @Inject(method = "writeStructureComponentsToNBT",
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/nbt/NBTTagCompound;setInteger(Ljava/lang/String;I)V", ordinal = 0),
+            locals = LocalCapture.CAPTURE_FAILHARD)
+    private void writeYToNbt(int chunkX, int chunkZ, CallbackInfoReturnable<NBTTagCompound> cir, NBTTagCompound tag) {
+        tag.setInteger("ChunkY", this.cubeY);
+    }
+
+    @Inject(method = "readStructureComponentsFromNBT", at = @At("HEAD"))
+    private void readYFromNBT(World world, NBTTagCompound tag, CallbackInfo cbi) {
+        if (tag.hasKey("ChunkY")) {
+            this.isCubic = true;
+            this.cubeY = tag.getInteger("ChunkY");
+        }
+    }
+
+    @Inject(method = "markAvailableHeight", at = @At("HEAD"), cancellable = true)
+    private void markAvailableHeightCubic(World worldIn, @Nullable Random rand, int minDepth, CallbackInfo cbi) {
+        if (!this.isCubic) {
+            if (((ICubicWorld) worldIn).isCubicWorld()) {
+                Preconditions.checkNotNull(rand);
+                this.markAvailableHeightDone = true;
+                this.minDepth = minDepth;
+                this.randY = rand.nextInt(ICube.SIZE);
+            }
+            return;
+        }
+        int maxY = Math.round(expectedBaseHeight - minDepth);
+        int originalY = this.boundingBox.minY;
+        int newY = localToBlock(getChunkPosY(), rand == null ? this.randY : rand.nextInt(ICube.SIZE));
+
+        int newYMaxBound = boundingBox.getYSize() + newY;
+        if (newYMaxBound > maxY) {
+            newY -= newYMaxBound - maxY;
+        }
+        int offset = newY - originalY;
+        // scrollOffset down by originalY, and then up to cube pos
+        this.boundingBox.offset(0, offset, 0);
+
+        for (StructureComponent component : this.components) {
+            component.offset(0, offset, 0);
+        }
+        if (cbi != null) {
+            cbi.cancel();
+        }
+    }
+
+    @Inject(method = "setRandomHeight", at = @At("HEAD"), cancellable = true)
+    private void setRandomHeightCubic(World worldIn, Random rand, int minY, int maxY, CallbackInfo cbi) {
+        if (!this.isCubic) {
+            return;
+        }
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override public int getX() {
+        return getChunkPosX();
+    }
+
+    @Override public int getY() {
+        return getChunkPosY();
+    }
+
+    @Override public int getZ() {
+        return getChunkPosZ();
+    }
+}
