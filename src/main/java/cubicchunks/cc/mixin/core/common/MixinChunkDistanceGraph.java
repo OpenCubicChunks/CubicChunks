7fb4a0bec2282cd0f1ed50d9e02d51bc8b0d0209
FoundSpore <aaronscene349@gmail.com>
2020-05-17 20:17:05 -0700

    Mixin Chunk SentinelSource Level to Long.MaxValue

---
+++ src/main/java/cubicchunks/cc/mixin/core/common/MixinChunkDistanceGraph.java
@@ -0,0 +1,18 @@
+package cubicchunks.cc.mixin.core.common;
+
+import cubicchunks.cc.mixin.core.common.ticket.interfaces.ILevelBasedGraph;
+import net.minecraft.world.chunk.ChunkDistanceGraph;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(ChunkDistanceGraph.class)
+public class MixinChunkDistanceGraph {
+
+    @Inject(method = "updateSourceLevel(JIZ)V", at = @At("HEAD"))
+    private void removeSentinelInSourceLevel(long pos, int level, boolean isDecreasing, CallbackInfo ci) {
+        ((ILevelBasedGraph)this).scheduleMixedUpdate(Long.MAX_VALUE, pos, level, isDecreasing);
+
+    }
+}
---
+++ src/main/java/cubicchunks/cc/mixin/core/common/ticket/interfaces/ILevelBasedGraph.java
@@ -0,0 +1,10 @@
+package cubicchunks.cc.mixin.core.common.ticket.interfaces;
+
+import net.minecraft.world.lighting.LevelBasedGraph;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Invoker;
+
+@Mixin(LevelBasedGraph.class)
+public interface ILevelBasedGraph {
+    @Invoker("scheduleUpdate") void scheduleMixedUpdate(long fromPos, long toPos, int newLevel, boolean isDecreasing);
+}
