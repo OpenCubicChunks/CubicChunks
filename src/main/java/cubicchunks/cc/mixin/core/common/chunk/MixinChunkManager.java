1ecd05c5fb24d34369c0bde5fa470194e69a8b27
NotStirred <notstirred9@gmail.com>
2020-05-18 22:27:42 +0100

    added MixinChunkManager

---
+++ src/main/java/cubicchunks/cc/mixin/core/common/chunk/MixinChunkManager.java
@@ -0,0 +1,9 @@
+package cubicchunks.cc.mixin.core.common.chunk;
+
+import net.minecraft.world.server.ChunkManager;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(ChunkManager.class)
+public abstract class MixinChunkManager {
+
+}
