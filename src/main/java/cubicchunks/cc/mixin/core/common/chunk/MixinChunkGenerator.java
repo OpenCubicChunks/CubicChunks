59137d52e02ad2d274ce3930888768e9a5ee5eb7
Barteks2x <Barteks2x@gmail.com>
2020-05-28 23:10:51 +0200

    ChunkStatus and ChunkGenerator fixes

--- src/main/java/cubicchunks/cc/mixin/core/common/chunk/MixinChunkGenerator.java
+++ src/main/java/cubicchunks/cc/mixin/core/common/chunk/MixinChunkGenerator.java
@@ -1,6 +1,7 @@
 package cubicchunks.cc.mixin.core.common.chunk;
 
 import cubicchunks.cc.chunk.ICubeGenerator;
+import net.minecraft.world.IWorld;
 import net.minecraft.world.biome.BiomeManager;
 import net.minecraft.world.chunk.IChunk;
 import net.minecraft.world.gen.ChunkGenerator;
@@ -15,10 +16,20 @@ public class MixinChunkGenerator implements ICubeGenerator {
 
     @Inject(method = "generateStructures", at = @At("HEAD"), cancellable = true)
     public void onGenerateStructures(BiomeManager p_227058_1_, IChunk p_227058_2_, ChunkGenerator<?> p_227058_3_, TemplateManager p_227058_4_,
-            CallbackInfo ci)
-    {
+            CallbackInfo ci) {
+
         ci.cancel();
-        return;
+    }
+
+
+    @Inject(method = "generateStructureStarts", at = @At("HEAD"), cancellable = true)
+    public void generateStructureStarts(IWorld worldIn, IChunk chunkIn, CallbackInfo ci) {
+        ci.cancel();
+    }
+
+    @Inject(method = "generateBiomes", at = @At("HEAD"), cancellable = true)
+    public void generateBiomes(IChunk chunkIn, CallbackInfo ci) {
+        ci.cancel();
     }
 
 }
--- src/main/java/cubicchunks/cc/mixin/core/common/chunk/MixinChunkStatus.java
+++ src/main/java/cubicchunks/cc/mixin/core/common/chunk/MixinChunkStatus.java
@@ -3,9 +3,12 @@ package cubicchunks.cc.mixin.core.common.chunk;
 import com.mojang.datafixers.util.Either;
 import cubicchunks.cc.chunk.ICube;
 import cubicchunks.cc.chunk.cube.CubePrimer;
+import net.minecraft.block.Blocks;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.chunk.ChunkStatus;
 import net.minecraft.world.chunk.IChunk;
 import net.minecraft.world.gen.ChunkGenerator;
+import net.minecraft.world.gen.GenerationStage;
 import net.minecraft.world.gen.feature.template.TemplateManager;
 import net.minecraft.world.server.ChunkHolder;
 import net.minecraft.world.server.ServerWorld;
@@ -13,6 +16,7 @@ import net.minecraft.world.server.ServerWorldLightManager;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.List;
@@ -22,8 +26,21 @@ import java.util.function.Function;
 @Mixin(ChunkStatus.class)
 public class MixinChunkStatus {
     // lambda$static$0 == NOOP_LOADING_WORKER
+    // lambda$static$1 == EMPTY
     // lambda$static$2 == STRUCTURE_STARTS
+    // lambda$static$3 == STRUCTURE_REFERENCES
+    // lambda$static$4 == BIOMES
+    // lambda$static$5 == NOISE
+    // lambda$static$6 == SURFACE
+    // lambda$static$7 == CARVERS
+    // lambda$static$8 == LIQUID_CARVERS
     // lambda$static$9 == FEATURES
+    // lambda$static$10 == LIGHT
+    // lambda$static$11 == LIGHT (loading worker)
+    // lambda$static$12 == SPAWM
+    // lambda$static$13 == HEIGHTMAPS
+    // lambda$static$14 == FULL
+    // lambda$static$15 == FULL (loading worker)
 
     @SuppressWarnings("UnresolvedMixinReference")
     @Inject(method = "lambda$static$0", at = @At("HEAD"))
@@ -33,11 +50,15 @@ public class MixinChunkStatus {
             Function<IChunk, CompletableFuture<Either<IChunk, ChunkHolder.IChunkLoadingError>>> func,
             IChunk chunk,
             CallbackInfoReturnable<CompletableFuture<Either<IChunk, ChunkHolder.IChunkLoadingError>>> ci) {
+
         if (chunk instanceof CubePrimer && !chunk.getStatus().isAtLeast(status)) {
             ((CubePrimer) chunk).setStatus(status);
         }
     }
 
+    // EMPTY -> does nothing already
+
+    // structure starts - replace setStatus, handled by MixinChunkGenerator
     @SuppressWarnings("UnresolvedMixinReference")
     @Inject(method = "lambda$static$2", at = @At("HEAD"), cancellable = true)
     private static void generateStructureStatus(
@@ -48,20 +69,93 @@ public class MixinChunkStatus {
             CallbackInfoReturnable<CompletableFuture<Either<IChunk, ChunkHolder.IChunkLoadingError>>> cir) {
 
         cir.setReturnValue(CompletableFuture.completedFuture(Either.left(chunk)));
-        if(!(chunk instanceof ICube))
-        {
+        if (!(chunk instanceof ICube)) {
             //vanilla
             return;
         }
         //cc
         if (!((ICube) chunk).getCubeStatus().isAtLeast(status)) {
             if (world.getWorldInfo().isMapFeaturesEnabled()) {
-                generator.generateStructures(world.getBiomeManager().copyWithProvider(generator.getBiomeProvider()), chunk, generator, templateManager);
+                generator.generateStructures(world.getBiomeManager().copyWithProvider(generator.getBiomeProvider()), chunk, generator,
+                        templateManager);
             }
 
             if (chunk instanceof CubePrimer) {
-                ((CubePrimer)chunk).setStatus(status);
+                ((CubePrimer) chunk).setStatus(status);
             }
+        }
+    }
+
+    @SuppressWarnings("UnresolvedMixinReference")
+    @Inject(method = "lambda$static$3", at = @At("HEAD"), cancellable = true)
+    private static void cubicChunksStructureReferences(ServerWorld world, ChunkGenerator<?> generator, List<IChunk> neighbors, IChunk chunk,
+            CallbackInfo ci) {
+
+        ci.cancel();
+        if (chunk instanceof ICube) {
+            // generator.generateStructureStarts(new CubeWorldGenRegion(world, unsafeCast(neighbors)), chunk);
+        }
+    }
+    // biomes -> handled by MixinChunkGenerator
+    @SuppressWarnings("UnresolvedMixinReference")
+    @Inject(method = "lambda$static$5", at = @At("HEAD"), cancellable = true)
+    private static void cubicChunksNoise(ServerWorld world, ChunkGenerator<?> generator, List<IChunk> neighbors, IChunk chunk,
+            CallbackInfo ci) {
+
+        ci.cancel();
+        if (chunk instanceof ICube) {
+            // generator.makeBase(new CubeWorldGenRegion(world, unsafeCast(neighbors)), chunk);
+            ICube cube = (ICube) chunk;
+            int cubeY = cube.getSectionPos().getY();
+            if (cubeY < 5) {
+                for (int x = 0; x < 16; x++) {
+                    for (int y = 0; y < 16; y++) {
+                        for (int z = 0; z < 16; z++) {
+                            BlockPos pos = new BlockPos(x, y, z);
+                            if (cubeY == 4 && y == 15) {
+                                cube.setBlockState(pos, Blocks.GRASS.getDefaultState(), false);
+                            } else if (cubeY == 0 && y == 0) {
+                                cube.setBlockState(pos, Blocks.BEDROCK.getDefaultState(), false);
+                            } else {
+                                cube.setBlockState(pos, Blocks.STONE.getDefaultState(), false);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    @SuppressWarnings("UnresolvedMixinReference")
+    @Inject(method = "lambda$static$6", at = @At("HEAD"), cancellable = true)
+    private static void cubicChunksSurface(ServerWorld world, ChunkGenerator<?> generator, List<IChunk> neighbors, IChunk chunk,
+            CallbackInfo ci) {
+
+        ci.cancel();
+        if (chunk instanceof ICube) {
+            // generator.generateSurface(new CubeWorldGenRegion(world, unsafeCast(neighbors)), chunk);
+        }
+    }
+
+    @SuppressWarnings("UnresolvedMixinReference")
+    @Inject(method = "lambda$static$7", at = @At("HEAD"), cancellable = true)
+    private static void cubicChunksCarvers(ServerWorld world, ChunkGenerator<?> generator, List<IChunk> neighbors, IChunk chunk,
+            CallbackInfo ci) {
+
+        ci.cancel();
+        if (chunk instanceof ICube) {
+            // generator.func_225550_a_(world.getBiomeManager().copyWithProvider(generator.getBiomeProvider()), chunk, GenerationStage.Carving.AIR);
+        }
+    }
+
+    @SuppressWarnings("UnresolvedMixinReference")
+    @Inject(method = "lambda$static$8", at = @At("HEAD"), cancellable = true)
+    private static void cubicChunksLiquidCarvers(ServerWorld world, ChunkGenerator<?> generator, List<IChunk> neighbors, IChunk chunk,
+            CallbackInfo ci) {
+
+        ci.cancel();
+        if (chunk instanceof ICube) {
+            // generator.func_225550_a_(world.getBiomeManager().copyWithProvider(generator.getBiomeProvider()), chunk, GenerationStage.Carving.LIQUID);
         }
     }
 
@@ -102,5 +196,15 @@ public class MixinChunkStatus {
         cir.setReturnValue(CompletableFuture.completedFuture(Either.left(chunk)));
     }
 
+    //lambda$static$12
+    @SuppressWarnings("UnresolvedMixinReference")
+    @Inject(method = "lambda$static$12", at = @At("HEAD"), cancellable = true)
+    private static void cubicChunksSpawnMobs(ServerWorld world, ChunkGenerator<?> generator, List<IChunk> neighbors, IChunk chunk,
+            CallbackInfo ci) {
 
+        ci.cancel();
+        if (chunk instanceof ICube) {
+            // generator.spawnMobs(new CubeWorldGenRegion(world, unsafeCast(neighbors)));
+        }
+    }
 }
