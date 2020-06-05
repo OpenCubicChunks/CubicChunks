8e50d99391503dff3536604a11d036f1c3c6966a
NotStirred <notstirred9@gmail.com>
2020-06-05 14:53:01 +0100

    removed death on player going to -64

---
+++ src/main/java/io/github/opencubicchunks/cubicchunks/mixin/core/common/entity/MixinEntity.java
@@ -0,0 +1,17 @@
+package io.github.opencubicchunks.cubicchunks.mixin.core.common.entity;
+
+import io.github.opencubicchunks.cubicchunks.CubicChunks;
+import net.minecraft.entity.Entity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
+
+@Mixin(Entity.class)
+public class MixinEntity {
+
+    @ModifyConstant(method = "baseTick", constant = @Constant(doubleValue = -64.0D))
+    private double getOutOfWorldPos(double _64)
+    {
+        return -CubicChunks.worldMAXHeight - 64;
+    }
+}
