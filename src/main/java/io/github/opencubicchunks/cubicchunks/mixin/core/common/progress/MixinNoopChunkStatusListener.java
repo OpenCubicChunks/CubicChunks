1617e75d6ee8e8ab8a61641d4d58e2f3e4203d9e
NotStirred <notstirred9@gmail.com>
2020-06-05 16:08:43 +0100

    fixed crash when casting NoopChunkStatusListener to ICubeStatusListener (on going into nether)

---
+++ src/main/java/io/github/opencubicchunks/cubicchunks/mixin/core/common/progress/MixinNoopChunkStatusListener.java
@@ -0,0 +1,25 @@
+package io.github.opencubicchunks.cubicchunks.mixin.core.common.progress;
+
+import io.github.opencubicchunks.cubicchunks.chunk.ICubeStatusListener;
+import net.minecraft.util.math.ChunkPos;
+import net.minecraft.world.chunk.ChunkStatus;
+import net.minecraftforge.common.DimensionManager;
+import org.spongepowered.asm.mixin.Mixin;
+
+import javax.annotation.Nullable;
+
+@Mixin(targets = "net.minecraftforge.common.DimensionManager$NoopChunkStatusListener")
+public class MixinNoopChunkStatusListener implements ICubeStatusListener {
+
+    @Override public void start(ChunkPos center) {
+
+    }
+
+    @Override public void statusChanged(ChunkPos chunkPosition, @Nullable ChunkStatus newStatus) {
+
+    }
+
+    @Override public void stop() {
+
+    }
+}
