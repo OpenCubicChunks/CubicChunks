3d34e2009094a7695fa3109115eaf392b069d192
Aidan Lovelace <aidan@aidanlovelace.com>
2020-06-20 22:15:24 -0500

    Vertical view distance slider in video settings - Properly updates renderer

--- src/main/java/io/github/opencubicchunks/cubicchunks/mixin/optifine/client/common/MixinWorldRenderer_Common.java
+++ src/main/java/io/github/opencubicchunks/cubicchunks/mixin/optifine/client/common/MixinWorldRenderer_Common.java
@@ -1,30 +1,42 @@
 package io.github.opencubicchunks.cubicchunks.mixin.optifine.client.common;
 
+import io.github.opencubicchunks.cubicchunks.CubicChunksConfig;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.WorldRenderer;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Constant;
-import org.spongepowered.asm.mixin.injection.ModifyConstant;
-import org.spongepowered.asm.mixin.injection.Redirect;
-import org.spongepowered.asm.mixin.injection.Slice;
+import org.spongepowered.asm.mixin.injection.*;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(WorldRenderer.class)
-public class MixinWorldRenderer_Common {
+public abstract class MixinWorldRenderer_Common {
 
     @Shadow @Final
     private Minecraft mc;
 
-    @Redirect(method = "setupTerrain", at = @At(value = "FIELD",
-            target = "Lnet/minecraft/client/renderer/WorldRenderer;renderDistanceChunks:I"),
-            slice = @Slice(from = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/WorldRenderer;loadRenderers()V")))
-    private int setUpterrain(WorldRenderer _this) {
-//        if (!((ICubicWorld) world).isCubicWorld()) {
-//            return mc.gameSettings.renderDistanceChunks;
-//        }
-        return mc.gameSettings.renderDistanceChunks;
+    @Shadow
+    private int renderDistanceChunks;
+
+    @Shadow public abstract void loadRenderers();
+
+    private int verticalRenderDistanceCubes = -1;
+
+    /**
+     * @author AidanLovelace
+     * @reason Make sure we load the renderers again if the vertical view distance changed but not if the normal render distance changed because then they'll be loaded again anyways.
+     */
+    @Inject(method = "setupTerrain", at = @At("HEAD"))
+    private void setupTerrainHEAD(CallbackInfo ci) {
+        if (this.renderDistanceChunks != mc.gameSettings.renderDistanceChunks) return;
+        if (this.verticalRenderDistanceCubes != CubicChunksConfig.verticalViewDistance.get()) {
+            this.loadRenderers();
+        }
+    }
+
+    @Inject(method = "loadRenderers", at = @At("HEAD"))
+    private void loadRenderersHEAD(CallbackInfo ci) {
+        this.verticalRenderDistanceCubes = CubicChunksConfig.verticalViewDistance.get();
     }
 
     @ModifyConstant(method = "renderWorldBorder", constant = @Constant(intValue = 256))
