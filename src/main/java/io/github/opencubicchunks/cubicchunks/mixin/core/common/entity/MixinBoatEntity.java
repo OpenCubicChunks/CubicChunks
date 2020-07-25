0a02d09b4e7147ca2dd3a1e6fe65a214f2d73eeb
OverInfrared <OverInfrared@users.noreply.github.com>
2020-07-25 16:49:58 -0700

    NotStirred had a bruh moment. Fixing bad classes.

--- src/main/java/io/github/opencubicchunks/cubicchunks/mixin/core/common/entity/MixinBoatEntity.java
+++ src/main/java/io/github/opencubicchunks/cubicchunks/mixin/core/common/entity/MixinBoatEntity.java
@@ -1,21 +1,12 @@
 package io.github.opencubicchunks.cubicchunks.mixin.core.common.entity;
 
-import io.github.opencubicchunks.cubicchunks.CubicChunks;
 import net.minecraft.entity.item.BoatEntity;
-import net.minecraft.entity.item.minecart.AbstractMinecartEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
 
-@Mixin(value = {AbstractMinecartEntity.class, BoatEntity.class})
-public class MixinVariousEntity {
-
-    /**
-     @author OverInfrared
-     @reason Fix boats and minecarts
-    **/
-    @ModifyConstant(method = "tick", constant = @Constant(doubleValue = -64.0D))
-    private double getOutOfWorldPos(double _64) { return -CubicChunks.worldMAXHeight - 64; }
+@Mixin(BoatEntity.class)
+public class MixinBoatEntity {
 
     @ModifyConstant(method = "checkInWater", constant = @Constant(doubleValue = Double.MIN_VALUE))
     private double waterLevelMinValue(double orig) {
