f611d882185bd3594b353ecd91d3543a8e8a7c11
CursedFlames <18627001+CursedFlames@users.noreply.github.com>
2020-07-26 21:18:25 +1200

    Make world load progress be based on cubes instead of columns

--- src/main/java/io/github/opencubicchunks/cubicchunks/mixin/core/common/progress/MixinLoggingChunkStatusListener.java
+++ src/main/java/io/github/opencubicchunks/cubicchunks/mixin/core/common/progress/MixinLoggingChunkStatusListener.java
@@ -1,21 +1,34 @@
 package io.github.opencubicchunks.cubicchunks.mixin.core.common.progress;
 
+import io.github.opencubicchunks.cubicchunks.chunk.IBigCube;
 import io.github.opencubicchunks.cubicchunks.chunk.ICubeStatusListener;
 import io.github.opencubicchunks.cubicchunks.chunk.util.CubePos;
-import net.minecraft.util.math.ChunkPos;
+import net.minecraft.util.math.MathHelper;
 import net.minecraft.world.chunk.ChunkStatus;
 import net.minecraft.world.chunk.listener.LoggingChunkStatusListener;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import javax.annotation.Nullable;
 
 @Mixin(LoggingChunkStatusListener.class)
 public abstract class MixinLoggingChunkStatusListener implements ICubeStatusListener {
+    private static final int CUBES_EDGE_LENGTH = ((((int) Math.ceil(10 * (16 / (float) IBigCube.BLOCK_SIZE)))*2)+3);
+    private static final int CUBES_TO_GENERATE = CUBES_EDGE_LENGTH*CUBES_EDGE_LENGTH*CUBES_EDGE_LENGTH;
 
-    @Shadow public abstract void statusChanged(ChunkPos chunkPosition, @Nullable ChunkStatus newStatus);
+    private int loadedCubes;
 
     @Override public void cubeStatusChanged(CubePos cubePos, @Nullable ChunkStatus newStatus) {
-        //this.statusChanged(chunkPosition.asChunkPos(), newStatus);
+        if (newStatus == ChunkStatus.FULL) {
+            this.loadedCubes++;
+        }
+    }
+
+    @Inject(method = "getPercentDone", at = @At("HEAD"), cancellable = true)
+    public void getPercentDone(CallbackInfoReturnable<Integer> cir) {
+        int percentage = MathHelper.floor(this.loadedCubes * 100.0F / CUBES_TO_GENERATE);
+        cir.setReturnValue(percentage);
     }
 }
