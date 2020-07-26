819e17ead00a25e95b75ade90cda6b65eaea8897
OverInfrared <OverInfrared@users.noreply.github.com>
2020-07-25 19:53:17 -0700

    Fixed Wither not spawning under Y:0

---
+++ src/main/java/io/github/opencubicchunks/cubicchunks/mixin/core/common/entity/MixinWitherSkeletonSkullBlock.java
@@ -0,0 +1,20 @@
+package io.github.opencubicchunks.cubicchunks.mixin.core.common.entity;
+
+import io.github.opencubicchunks.cubicchunks.CubicChunks;
+import net.minecraft.block.WitherSkeletonSkullBlock;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
+
+@Mixin(WitherSkeletonSkullBlock.class)
+public class MixinWitherSkeletonSkullBlock {
+
+    @ModifyConstant(method = "canSpawnMob", constant = @Constant(intValue = 2))
+    private static int getWitherSpawnCan(int _2) { return -CubicChunks.worldMAXHeight + 2; }
+
+    @ModifyConstant(method = "checkWitherSpawn", constant = @Constant(ordinal = 0, intValue = 2))
+    private static int getWitherSpawnCheck(int _2) {
+        return -CubicChunks.worldMAXHeight;
+    }
+
+}
