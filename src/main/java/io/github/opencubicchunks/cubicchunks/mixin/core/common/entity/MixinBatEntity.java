d6a1b6005d04158db7be0126c7262159802c0c2f
OverInfrared <OverInfrared@users.noreply.github.com>
2020-07-28 14:07:08 -0700

    Fixed bats not flying under Y:1

---
+++ src/main/java/io/github/opencubicchunks/cubicchunks/mixin/core/common/entity/MixinBatEntity.java
@@ -0,0 +1,15 @@
+package io.github.opencubicchunks.cubicchunks.mixin.core.common.entity;
+
+import io.github.opencubicchunks.cubicchunks.CubicChunks;
+import net.minecraft.entity.passive.BatEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
+
+@Mixin(BatEntity.class)
+public class MixinBatEntity {
+
+    @ModifyConstant(method = "updateAITasks", constant = @Constant(intValue = 1, ordinal = 0))
+    private int getUpdateAIWorldHeight(int _1) { return -CubicChunks.worldMAXHeight; }
+
+}
