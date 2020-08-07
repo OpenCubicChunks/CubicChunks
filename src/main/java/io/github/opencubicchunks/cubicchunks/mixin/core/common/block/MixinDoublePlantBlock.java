f961bcd15d37bf95bed3ec03582b897f03f897b5
CursedFlames <18627001+CursedFlames@users.noreply.github.com>
2020-08-07 17:40:22 +1200

    fix tall flower placement above y=255

---
+++ src/main/java/io/github/opencubicchunks/cubicchunks/mixin/core/common/block/MixinDoublePlantBlock.java
@@ -0,0 +1,19 @@
+package io.github.opencubicchunks.cubicchunks.mixin.core.common.block;
+
+import io.github.opencubicchunks.cubicchunks.CubicChunks;
+import net.minecraft.block.DoublePlantBlock;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
+
+@Mixin(DoublePlantBlock.class)
+public class MixinDoublePlantBlock {
+
+    /**
+     @author CursedFlames
+     @reason Fix tall flower placement above 255
+     **/
+    @ModifyConstant(method = "getStateForPlacement", constant = @Constant(intValue = 255))
+    private int getOutOfWorldMaxPos(int _255) { return CubicChunks.MAX_SUPPORTED_HEIGHT; }
+
+}
