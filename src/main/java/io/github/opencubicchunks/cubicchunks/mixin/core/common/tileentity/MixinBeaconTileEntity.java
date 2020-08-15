c01c067f7d7d707ea097b540c68a264280fc2c5e
NotStirred <notstirred9@gmail.com>
2020-08-15 06:32:44 +0100

    Fixed beacons generating infinite cubes Y. Added javadoc for BeaconTileEntity and ConduitTileEntity

--- src/main/java/io/github/opencubicchunks/cubicchunks/mixin/core/common/tileentity/MixinBeaconTileEntity.java
+++ src/main/java/io/github/opencubicchunks/cubicchunks/mixin/core/common/tileentity/MixinBeaconTileEntity.java
@@ -1,18 +1,17 @@
 package io.github.opencubicchunks.cubicchunks.mixin.core.common.tileentity;
 
-import net.minecraft.tileentity.ConduitTileEntity;
+import net.minecraft.tileentity.BeaconTileEntity;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
-@Mixin(ConduitTileEntity.class)
-public class MixinConduitTileEntity {
+@Mixin(BeaconTileEntity.class)
+public class MixinBeaconTileEntity {
     /**
-     * @author NotStirred
-     * @reason Conduits now only affect players within +256 of the block. This prevents near infinite cube loading in the conduit's column
+     * @author OverInfrared & NotStirred
+     * @reason Beacons now only affect players within +256 of the block. This prevents near infinite cube loading in the beacon's column
      */
-    //TODO: NotStirred merge this with BeaconTileEntity to reduce duplicated code
     @Redirect(method = {"addEffectsToPlayers()V"}, at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;getHeight()I"))
     private int on$getHeight(World world) {
         return 256;
