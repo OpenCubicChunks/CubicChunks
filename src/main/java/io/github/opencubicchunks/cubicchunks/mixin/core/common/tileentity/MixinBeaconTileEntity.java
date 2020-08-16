be027fa084d18c9406638d99eadcbd4d741c5411
NotStirred <notstirred9@gmail.com>
2020-08-17 00:51:27 +0100

    Added workaround due to mixin bug https://discordapp.com/channels/142425412096491520/626802111455297538/744601343943114893

--- src/main/java/io/github/opencubicchunks/cubicchunks/mixin/core/common/tileentity/MixinBeaconTileEntity.java
+++ src/main/java/io/github/opencubicchunks/cubicchunks/mixin/core/common/tileentity/MixinBeaconTileEntity.java
@@ -1,19 +1,28 @@
 package io.github.opencubicchunks.cubicchunks.mixin.core.common.tileentity;
 
+import io.github.opencubicchunks.cubicchunks.CubicChunks;
 import net.minecraft.tileentity.BeaconTileEntity;
-import net.minecraft.world.World;
+import net.minecraft.util.math.AxisAlignedBB;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
+
 @Mixin(BeaconTileEntity.class)
-public class MixinBeaconTileEntity {
+public abstract class MixinBeaconTileEntity {
     /**
      * @author OverInfrared & NotStirred
      * @reason Beacons now only affect players within +256 of the block. This prevents near infinite cube loading in the beacon's column
      */
-    @Redirect(method = {"addEffectsToPlayers()V"}, at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;getHeight()I"))
-    private int on$getHeight(World world) {
-        return 256;
+    //COMMENTED OUT BECAUSE OF A MIXIN BUG. It's not recursively checking interfaces, only the first
+//    @Redirect(method = {"addEffectsToPlayers()V"}, at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;getHeight()I", args="log=true"))
+//    private int on$getHeight(World world) {
+//        return 256;
+//    }
+    @Redirect(method = "addEffectsToPlayers", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/math/AxisAlignedBB;expand(DDD)Lnet/minecraft/util/math/AxisAlignedBB;"))
+    private AxisAlignedBB on$expand(AxisAlignedBB axisAlignedBB, double x, double y, double z) {
+        CubicChunks.LOGGER.warn("on$expand called");
+        return axisAlignedBB.expand(x, 256, z);
     }
+
 }
