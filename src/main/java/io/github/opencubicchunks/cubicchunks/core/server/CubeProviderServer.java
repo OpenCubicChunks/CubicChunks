e9b4fb69e875dca931dd62e8a588d7172efb6bf7
Barteks2x <Barteks2x@gmail.com>
2020-10-07 19:24:13 +0200

    Decrease load radius for first light spread, as edge light updates work now

--- src/main/java/io/github/opencubicchunks/cubicchunks/core/server/CubeProviderServer.java
+++ src/main/java/io/github/opencubicchunks/cubicchunks/core/server/CubeProviderServer.java
@@ -484,9 +484,9 @@ public class CubeProviderServer extends ChunkProviderServer implements ICubeProv
         int cubeZ = cube.getZ();
 
         // TODO: remove this loop; Does it break anything?
-        for (int x = -2; x <= 2; x++) {
-            for (int z = -2; z <= 2; z++) {
-                for (int y = 2; y >= -2; y--) {
+        for (int x = -1; x <= 1; x++) {
+            for (int z = -1; z <= 1; z++) {
+                for (int y = 1; y >= -1; y--) {
                     if (x != 0 || y != 0 || z != 0) {
                         getCube(x + cubeX, y + cubeY, z + cubeZ);
                     }
