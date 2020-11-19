a54f1af51904c30d6a5bf0e52fad3cc076868e8e
CorgiTaco <66983020+CorgiTaco@users.noreply.github.com>
2020-11-19 11:19:18 -0800

    -Disable Spring feature

---
+++ src/main/java/io/github/opencubicchunks/cubicchunks/mixin/core/common/world/feature/MixinSpringFeature.java
@@ -0,0 +1,23 @@
+package io.github.opencubicchunks.cubicchunks.mixin.core.common.world.feature;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.WorldGenLevel;
+import net.minecraft.world.level.chunk.ChunkGenerator;
+import net.minecraft.world.level.levelgen.feature.SpringFeature;
+import net.minecraft.world.level.levelgen.feature.configurations.SpringConfiguration;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.Random;
+
+//Enable this
+@Mixin(SpringFeature.class)
+public class MixinSpringFeature {
+
+    @Inject(at = @At("HEAD"), method = "place", cancellable = true)
+    private void cancel(WorldGenLevel worldGenLevel, ChunkGenerator chunkGenerator, Random random, BlockPos blockPos, SpringConfiguration springConfiguration, CallbackInfoReturnable<Boolean> cir) {
+        cir.setReturnValue(true);
+    }
+}
