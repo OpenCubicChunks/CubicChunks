c0f464870fa76e628f03fde1bc387a5ea61db1e2
CorgiTaco <66983020+CorgiTaco@users.noreply.github.com>
2020-11-21 15:19:16 -0800

    Check if sea level is within cube Y bounds for iceberg

---
+++ src/main/java/io/github/opencubicchunks/cubicchunks/mixin/core/common/world/feature/MixinIcebergFeature.java
@@ -0,0 +1,25 @@
+package io.github.opencubicchunks.cubicchunks.mixin.core.common.world.feature;
+
+import io.github.opencubicchunks.cubicchunks.chunk.IBigCube;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.WorldGenLevel;
+import net.minecraft.world.level.chunk.ChunkGenerator;
+import net.minecraft.world.level.levelgen.feature.IcebergFeature;
+import net.minecraft.world.level.levelgen.feature.configurations.BlockStateConfiguration;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.Random;
+
+@Mixin(IcebergFeature.class)
+public class MixinIcebergFeature {
+
+    @Inject(method = "place", at = @At("HEAD"), cancellable = true)
+    private void checkIfSeaLevelIsInCube(WorldGenLevel worldGenLevel, ChunkGenerator chunkGenerator, Random random, BlockPos blockPos, BlockStateConfiguration blockStateConfiguration, CallbackInfoReturnable<Boolean> cir) {
+        if (blockPos.getY() > chunkGenerator.getSeaLevel() || blockPos.getY() + IBigCube.DIAMETER_IN_BLOCKS <= chunkGenerator.getSeaLevel()) {
+            cir.setReturnValue(true);
+        }
+    }
+}
