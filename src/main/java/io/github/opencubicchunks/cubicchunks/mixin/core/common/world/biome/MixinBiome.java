6f92f237a433b54027f2b46c70cf23ff30584d48
CorgiTaco <66983020+CorgiTaco@users.noreply.github.com>
2020-11-21 15:20:30 -0800

    Enable all biome features besides a select few.

--- src/main/java/io/github/opencubicchunks/cubicchunks/mixin/core/common/world/biome/MixinBiome.java
+++ src/main/java/io/github/opencubicchunks/cubicchunks/mixin/core/common/world/biome/MixinBiome.java
@@ -13,7 +13,6 @@ import net.minecraft.ReportedException;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Registry;
 import net.minecraft.core.SectionPos;
-import net.minecraft.data.BuiltinRegistries;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.StructureFeatureManager;
 import net.minecraft.world.level.biome.Biome;
@@ -41,7 +40,7 @@ public class MixinBiome implements BiomeGetter {
     private Map<Integer, List<StructureFeature<?>>> structuresByStep;
 
 
-    private final Set<ResourceLocation> featureIDWhitelist = new HashSet<>();
+    private final Set<ResourceLocation> featureBlacklist = new HashSet<>();
 
     @Override
     public void generate(StructureFeatureManager structureFeatureManager, ChunkGenerator chunkGenerator, CubeWorldGenRegion cubeWorldGenRegion, long seed, CubeWorldGenRandom worldgenRandom, BlockPos blockPos) {
@@ -72,8 +71,8 @@ public class MixinBiome implements BiomeGetter {
                 }
             }
 
-            if (featureIDWhitelist.isEmpty())
-                getWhitelist(cubeWorldGenRegion);
+            if (featureBlacklist.isEmpty())
+                getBlacklist(cubeWorldGenRegion);
 
 
             if (list.size() > genStepIDX) {
@@ -90,7 +89,7 @@ public class MixinBiome implements BiomeGetter {
 
                     ConfiguredFeature<?, ?> configuredFeature1 = configuredFeature;
 
-                    if (featureIDWhitelist.contains(key) || genStepIDX == GenerationStep.Decoration.VEGETAL_DECORATION.ordinal() || genStepIDX == GenerationStep.Decoration.TOP_LAYER_MODIFICATION.ordinal()) {
+                    if (!featureBlacklist.contains(key)) {
                         try {
                             configuredFeature.place(cubeWorldGenRegion, chunkGenerator, worldgenRandom, blockPos);
                         } catch (Exception e) {
@@ -107,146 +106,49 @@ public class MixinBiome implements BiomeGetter {
         }
     }
 
-    private void getWhitelist(CubeWorldGenRegion cubeWorldGenRegion) {
+    //TODO: Remove this blacklist.
+    private void getBlacklist(CubeWorldGenRegion cubeWorldGenRegion) {
 
         List<ResourceLocation> resourceLocationList = Arrays.asList(
-                //Trees
-                new ResourceLocation("oak"),
-                new ResourceLocation("dark_oak"),
-                new ResourceLocation("birch"),
-                new ResourceLocation("acacia"),
-                new ResourceLocation("spruce"),
-                new ResourceLocation("pine"),
-                new ResourceLocation("jungle_tree"),
-                new ResourceLocation("fancy_oak"),
-                new ResourceLocation("jungle_tree_no_vine"),
-                new ResourceLocation("mega_jungle_tree"),
-                new ResourceLocation("mega_spruce"),
-                new ResourceLocation("mega_pine"),
-                new ResourceLocation("super_birch_bees_0002"),
-                new ResourceLocation("swamp_tree"),
-                new ResourceLocation("jungle_bush"),
-                new ResourceLocation("oak_bees_0002"),
-                new ResourceLocation("oak_bees_002"),
-                new ResourceLocation("oak_bees_005"),
-                new ResourceLocation("birch_bees_0002"),
-                new ResourceLocation("birch_bees_002"),
-                new ResourceLocation("birch_bees_005"),
-                new ResourceLocation("fancy_oak_bees_0002"),
-                new ResourceLocation("fancy_oak_bees_002"),
-                new ResourceLocation("fancy_oak_bees_005"),
-                new ResourceLocation("oak_badlands"),
-                new ResourceLocation("spruce_snowy"),
-                new ResourceLocation("flower_warm"),
-                new ResourceLocation("flower_default"),
-                new ResourceLocation("flower_forest"),
-                new ResourceLocation("flower_swamp"),
-                new ResourceLocation("flower_plain"),
-                new ResourceLocation("flower_plain_decorated"),
-                new ResourceLocation("forest_flower_vegetation_common"),
-                new ResourceLocation("forest_flower_vegetation"),
 
-                //Trees(Random Selectors)
-                new ResourceLocation("forest_flower_trees"),
-                new ResourceLocation("taiga_vegetation"),
-                new ResourceLocation("trees_shattered_savanna"),
-                new ResourceLocation("trees_savanna"),
-                new ResourceLocation("birch_tall"),
-                new ResourceLocation("trees_birch"),
-                new ResourceLocation("trees_mountain_edge"),
-                new ResourceLocation("trees_mountain"),
-                new ResourceLocation("trees_water"),
-                new ResourceLocation("birch_other"),
-                new ResourceLocation("plain_vegetation"),
-                new ResourceLocation("trees_jungle_edge"),
-                new ResourceLocation("trees_giant_spruce"),
-                new ResourceLocation("trees_giant"),
-                new ResourceLocation("trees_jungle"),
-                new ResourceLocation("dark_forest_vegetation_brown"),
-                new ResourceLocation("dark_forest_vegetation_red"),
-                new ResourceLocation("warm_ocean_vegetation"),
-                new ResourceLocation("forest_flower_vegetation_common"),
-                new ResourceLocation("mushroom_field_vegetation"),
+                //Broken Features
+                new ResourceLocation("ice_spike"), /**{@link net.minecraft.world.level.levelgen.feature.IceSpikeFeature}**/ //Handles its placement in its own class w/ a while loop.
+                new ResourceLocation("spring_water"), //Requires similar 1.12 implementation, see: https://github.com/OpenCubicChunks/CubicWorldGen/blob/27de56d2f792513873584b2f8fd9f3082fb259ec/src/main/java/io/github/opencubicchunks/cubicchunks/cubicgen/customcubic/populator/DefaultDecorator.java#L331-L361
+                new ResourceLocation("spring_lava"), //Requires similar 1.12 implementation, see: https://github.com/OpenCubicChunks/CubicWorldGen/blob/27de56d2f792513873584b2f8fd9f3082fb259ec/src/main/java/io/github/opencubicchunks/cubicchunks/cubicgen/customcubic/populator/DefaultDecorator.java#L331-L361
+                new ResourceLocation("seagrass_simple"), //Requires Carving mask
+                new ResourceLocation("fossil"),
+                new ResourceLocation("desert_well"), //Iterates downwards in its placement
+                new ResourceLocation("ice_patch"), //Iterates downwards in its placement
 
-                //Random Patches
-                new ResourceLocation("patch_fire"),
-                new ResourceLocation("patch_soul_fire"),
-                new ResourceLocation("patch_brown_mushroom"),
-                new ResourceLocation("patch_red_mushroom"),
-                new ResourceLocation("patch_crimson_roots"),
-                new ResourceLocation("patch_sunflower"),
-                new ResourceLocation("patch_pumpkin"),
-                new ResourceLocation("patch_taiga_grass"),
-                new ResourceLocation("patch_berry_bush"),
-                new ResourceLocation("patch_grass_plain"),
-                new ResourceLocation("patch_grass_forest"),
-                new ResourceLocation("patch_grass_badlands"),
-                new ResourceLocation("patch_grass_savanna"),
-                new ResourceLocation("patch_grass_normal"),
-                new ResourceLocation("patch_grass_taiga_2"),
-                new ResourceLocation("patch_grass_taiga"),
-                new ResourceLocation("patch_grass_jungle"),
-                new ResourceLocation("patch_dead_bush_2"),
-                new ResourceLocation("patch_dead_bush"),
-                new ResourceLocation("patch_dead_bush_badlands"),
-                new ResourceLocation("patch_melon"),
-                new ResourceLocation("patch_berry_sparse"),
-                new ResourceLocation("patch_berry_decorated"),
-                new ResourceLocation("patch_waterlilly"),
-                new ResourceLocation("patch_tall_grass_2"),
-                new ResourceLocation("patch_tall_grass"),
-                new ResourceLocation("patch_large_fern"),
-                new ResourceLocation("patch_cactus"),
-                new ResourceLocation("patch_cactus_desert"),
-                new ResourceLocation("patch_cactus_decorated"),
-                new ResourceLocation("patch_sugar_cane_swamp"),
-                new ResourceLocation("patch_sugar_cane_desert"),
-                new ResourceLocation("patch_sugar_cane_badlands"),
-                new ResourceLocation("patch_sugar_cane"),
 
-                //Lakes
-                new ResourceLocation("lake_lava"),
-                new ResourceLocation("lake_water")
-
-//                    Ores //TODO: OPTIMIZE ORE GEN
-//                    new ResourceLocation("ore_magma"),
-//                    new ResourceLocation("ore_soul_sand"),
-//                    new ResourceLocation("ore_gold_deltas"),
-//                    new ResourceLocation("ore_quartz_deltas"),
-//                    new ResourceLocation("ore_gold_nether"),
-//                    new ResourceLocation("ore_quartz_nether"),
-//                    new ResourceLocation("ore_gravel_nether"),
-//                    new ResourceLocation("ore_blackstone"),
-//                    new ResourceLocation("ore_dirt"),
-//                    new ResourceLocation("ore_gravel"),
-//                    new ResourceLocation("ore_granite"),
-//                    new ResourceLocation("ore_diorite"),
-//                    new ResourceLocation("ore_andesite"),
-//                    new ResourceLocation("ore_coal"),
-//                    new ResourceLocation("ore_iron"),
-//                    new ResourceLocation("ore_gold_extra"),
-//                    new ResourceLocation("ore_gold"),
-//                    new ResourceLocation("ore_redstone"),
-//                    new ResourceLocation("ore_diamond"),
-//                    new ResourceLocation("ore_lapis"),
-//                    new ResourceLocation("ore_infested"),
-//                    new ResourceLocation("ore_emerald"),
-//                    new ResourceLocation("ore_debris_large"),
-//                    new ResourceLocation("ore_debris_small"),
-//                    new ResourceLocation("ore_copper")
-
+                //Ores //TODO: OPTIMIZE ORE GEN
+                new ResourceLocation("ore_magma"),
+                new ResourceLocation("ore_soul_sand"),
+                new ResourceLocation("ore_gold_deltas"),
+                new ResourceLocation("ore_quartz_deltas"),
+                new ResourceLocation("ore_gold_nether"),
+                new ResourceLocation("ore_quartz_nether"),
+                new ResourceLocation("ore_gravel_nether"),
+                new ResourceLocation("ore_blackstone"),
+                new ResourceLocation("ore_dirt"),
+                new ResourceLocation("ore_gravel"),
+                new ResourceLocation("ore_granite"),
+                new ResourceLocation("ore_diorite"),
+                new ResourceLocation("ore_andesite"),
+                new ResourceLocation("ore_coal"),
+                new ResourceLocation("ore_iron"),
+                new ResourceLocation("ore_gold_extra"),
+                new ResourceLocation("ore_gold"),
+                new ResourceLocation("ore_redstone"),
+                new ResourceLocation("ore_diamond"),
+                new ResourceLocation("ore_lapis"),
+                new ResourceLocation("ore_infested"),
+                new ResourceLocation("ore_emerald"),
+                new ResourceLocation("ore_debris_large"),
+                new ResourceLocation("ore_debris_small"),
+                new ResourceLocation("ore_copper")
         );
 
-        for (ResourceLocation keyFromRegistry : cubeWorldGenRegion.getLevel().getServer().registryAccess().registry(Registry.CONFIGURED_FEATURE_REGISTRY).get().keySet()) {
-            if (keyFromRegistry.getNamespace().equals("byg"))
-                CubicChunks.LOGGER.info("BYG: " + keyFromRegistry);
-
-            if (keyFromRegistry.toString().contains("tree"))
-                featureIDWhitelist.add(keyFromRegistry);
-        }
-        CubicChunks.LOGGER.info("==================================Finished----------------================--");
-
-
-        featureIDWhitelist.addAll(resourceLocationList);
+        featureBlacklist.addAll(resourceLocationList);
     }
 }
