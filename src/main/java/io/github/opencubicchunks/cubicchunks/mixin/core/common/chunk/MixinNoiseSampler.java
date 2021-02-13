3d869c8ddd5b3d5816741c6f1d35959a1ce701e3
CorgiTaco <66983020+CorgiTaco@users.noreply.github.com>
2021-02-13 14:06:43 -0800

    -Let island noise slide

--- src/main/java/io/github/opencubicchunks/cubicchunks/mixin/core/common/chunk/MixinNoiseSampler.java
+++ src/main/java/io/github/opencubicchunks/cubicchunks/mixin/core/common/chunk/MixinNoiseSampler.java
@@ -1,7 +1,10 @@
 package io.github.opencubicchunks.cubicchunks.mixin.core.common.chunk;
 
 import net.minecraft.world.level.levelgen.NoiseSampler;
+import net.minecraft.world.level.levelgen.NoiseSettings;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
@@ -9,9 +12,14 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(NoiseSampler.class)
 public class MixinNoiseSampler {
 
+    @Shadow @Final private NoiseSettings noiseSettings;
 
     @Inject(method = "applySlide", at = @At("HEAD"), cancellable = true)
-    private void dontSlideInfiniteCubeGen(double noise, int y, CallbackInfoReturnable<Double> cir) {
+    private void onlySlideIslandNoise(double noise, int y, CallbackInfoReturnable<Double> cir) {
+        if (noiseSettings.islandNoiseOverride()) { //If we're in the end, let it slide
+            return;
+        }
+
         cir.setReturnValue(noise);
     }
 }
