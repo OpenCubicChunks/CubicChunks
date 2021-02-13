4ab25497f0fc04a5e43f3831091111880f138fb7
CorgiTaco <66983020+CorgiTaco@users.noreply.github.com>
2021-02-13 14:56:11 -0800

    -Entity tracker start

---
+++ src/main/java/io/github/opencubicchunks/cubicchunks/chunk/ImposterChunkPos.java
@@ -0,0 +1,22 @@
+package io.github.opencubicchunks.cubicchunks.chunk;
+
+import io.github.opencubicchunks.cubicchunks.chunk.util.CubePos;
+import net.minecraft.world.level.ChunkPos;
+
+public class ImposterChunkPos extends ChunkPos {
+    public final int y;
+
+    public ImposterChunkPos(int x, int y, int z) {
+        super(x, z);
+        this.y = y;
+    }
+
+    public ImposterChunkPos(CubePos cubePos) {
+        this(cubePos.getX(), cubePos.getY(), cubePos.getZ());
+    }
+
+
+    public CubePos toCubePos() {
+        return CubePos.of(this.x, this.y, this.z);
+    }
+}
