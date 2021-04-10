c03fdf4d292929bb3daef39b949dd4dd7f5f633b
Tom Martin <tom.martin1239@gmail.com>
2021-04-10 12:55:08 +0100

    Add debug chunk&cube custom load order (#662)

--- src/main/java/io/github/opencubicchunks/cubicchunks/CubicChunks.java
+++ src/main/java/io/github/opencubicchunks/cubicchunks/CubicChunks.java
@@ -12,6 +12,7 @@ import io.github.opencubicchunks.cubicchunks.world.biome.StripedBiomeSource;
 import io.github.opencubicchunks.cubicchunks.world.gen.feature.CCFeatures;
 import io.github.opencubicchunks.cubicchunks.world.gen.placement.CCPlacement;
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.SharedConstants;
 import net.minecraft.Util;
 import net.minecraft.core.Registry;
 import net.minecraft.resources.ResourceLocation;
@@ -56,6 +57,7 @@ public class CubicChunks implements ModInitializer {
         if (System.getProperty("cubicchunks.debug", "false").equalsIgnoreCase("true")) {
             try {
                 Class.forName("io.github.opencubicchunks.cubicchunks.debug.DebugVisualization").getMethod("enable").invoke(null);
+                SharedConstants.IS_RUNNING_IN_IDE = true;
             } catch (IllegalAccessException | InvocationTargetException | NoSuchMethodException | ClassNotFoundException e) {
                 LOGGER.catching(e);
             }
