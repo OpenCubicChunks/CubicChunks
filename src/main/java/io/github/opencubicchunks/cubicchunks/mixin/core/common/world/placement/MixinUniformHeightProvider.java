3a733df6b3499b3dc2df8dc5cbede7a2dfbd0759
Corgi Taco <66983020+CorgiTaco@users.noreply.github.com>
2021-04-15 17:54:20 -0700

    Actually use minY

--- src/main/java/io/github/opencubicchunks/cubicchunks/mixin/core/common/world/placement/MixinUniformHeightProvider.java
+++ src/main/java/io/github/opencubicchunks/cubicchunks/mixin/core/common/world/placement/MixinUniformHeightProvider.java
@@ -8,6 +8,7 @@ import io.github.opencubicchunks.cubicchunks.world.gen.placement.CubicHeightProv
 import net.minecraft.world.level.levelgen.VerticalAnchor;
 import net.minecraft.world.level.levelgen.WorldGenerationContext;
 import net.minecraft.world.level.levelgen.heightproviders.UniformHeight;
+import net.minecraft.world.level.levelgen.placement.DecorationContext;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -19,9 +20,9 @@ public abstract class MixinUniformHeightProvider implements CubicHeightProvider 
 
     @Shadow @Final private VerticalAnchor minInclusive;
 
-    @Override public OptionalInt sampleCubic(Random rand, WorldGenerationContext context, int cubeMinY, int maxY) {
+    @Override public OptionalInt sampleCubic(Random rand, WorldGenerationContext context, int cubeMinY, int cubeMaxY) {
         int minInclusive = this.minInclusive.resolveY(context);
-        double bottomOffset = minInclusive == 0 ? Double.NEGATIVE_INFINITY : minInclusive;
+        double bottomOffset = minInclusive == (((DecorationContext) context).getLevel()).getLevel().dimensionType().minY() ? Double.NEGATIVE_INFINITY : minInclusive;
 
         int maxInclusive = this.maxInclusive.resolveY(context);
         double probability = 1.0 / ((maxInclusive - minInclusive + 1) / (256.0 / IBigCube.DIAMETER_IN_BLOCKS));
@@ -31,7 +32,7 @@ public abstract class MixinUniformHeightProvider implements CubicHeightProvider 
         }
 
         double maxBlockY = maxInclusive + 1;
-        int blockY = rand.nextInt(maxY - cubeMinY) + cubeMinY/*Equivalent to cubepos.getMinBlockY in 1.12*/;
+        int blockY = rand.nextInt(cubeMaxY - cubeMinY) + cubeMinY/*Equivalent to cubepos.getMinBlockY in 1.12*/;
 
         if (blockY > maxBlockY || blockY < bottomOffset) {
             return OptionalInt.empty();
