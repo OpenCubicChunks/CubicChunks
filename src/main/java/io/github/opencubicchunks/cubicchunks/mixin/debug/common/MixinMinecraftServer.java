970b2555b6bb8d9b20948c43464fba885638e983
Tom Martin <tom.martin1239@gmail.com>
2021-04-19 23:45:51 +0100

    debug load order now adds tickets and prevents adding player tickets

--- src/main/java/io/github/opencubicchunks/cubicchunks/mixin/debug/common/MixinMinecraftServer.java
+++ src/main/java/io/github/opencubicchunks/cubicchunks/mixin/debug/common/MixinMinecraftServer.java
@@ -3,7 +3,9 @@ package io.github.opencubicchunks.cubicchunks.mixin.debug.common;
 import java.util.Iterator;
 import java.util.Map;
 
-import io.github.opencubicchunks.cubicchunks.server.ICubicWorld;
+import io.github.opencubicchunks.cubicchunks.CubicChunks;
+import io.github.opencubicchunks.cubicchunks.chunk.util.CubePos;
+import io.github.opencubicchunks.cubicchunks.server.IServerChunkProvider;
 import it.unimi.dsi.fastutil.longs.LongIterator;
 import net.minecraft.Util;
 import net.minecraft.core.BlockPos;
@@ -11,12 +13,12 @@ import net.minecraft.resources.ResourceKey;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.level.ServerChunkCache;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.server.level.TicketType;
 import net.minecraft.server.level.progress.ChunkProgressListener;
+import net.minecraft.util.Unit;
 import net.minecraft.world.level.ChunkPos;
 import net.minecraft.world.level.ForcedChunksSavedData;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.chunk.ChunkStatus;
-import org.apache.logging.log4j.Logger;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -28,17 +30,28 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public abstract class MixinMinecraftServer {
     private static final boolean DEBUG_LOAD_ORDER_ENABLED = System.getProperty("cubicchunks.debug.loadorder", "false").equals("true");
 
-    @Shadow @Final private static Logger LOGGER;
-
-    @Shadow private long nextTickTime;
+    private int count = 0;
 
     @Shadow @Final private Map<ResourceKey<Level>, ServerLevel> levels;
+
+    @Shadow private long nextTickTime;
 
     @Shadow protected abstract void waitUntilNextTick();
 
     @Shadow protected abstract void updateMobSpawningFlags();
 
     @Shadow public abstract ServerLevel overworld();
+
+    @Shadow public abstract boolean isRunning();
+
+    private void addChunk(ServerChunkCache serverChunkCache, ChunkPos pos) {
+        serverChunkCache.addRegionTicket(TicketType.START, pos, 1, Unit.INSTANCE);
+        count++;
+    }
+    private void addCube(IServerChunkProvider serverChunkProvider, CubePos pos) {
+        serverChunkProvider.addCubeRegionTicket(TicketType.START, pos, 1, Unit.INSTANCE);
+        count++;
+    }
 
     /**
      * @author NotStirred
@@ -52,35 +65,30 @@ public abstract class MixinMinecraftServer {
 
         ci.cancel();
         ServerLevel serverLevel = this.overworld();
-        LOGGER.info("Preparing start region for dimension {}", serverLevel.dimension().location());
+        CubicChunks.LOGGER.info("Preparing start region for dimension {}", serverLevel.dimension().location());
         BlockPos blockPos = serverLevel.getSharedSpawnPos();
         worldGenerationProgressListener.updateSpawnPos(new ChunkPos(blockPos));
         ServerChunkCache serverChunkCache = serverLevel.getChunkSource();
         serverChunkCache.getLightEngine().setTaskPerBatch(500);
         this.nextTickTime = Util.getMillis();
 
-        overworld().getChunk(0, 0, ChunkStatus.FULL, true);
-        overworld().getChunk(-5, -5, ChunkStatus.FULL, true);
-        overworld().getChunk(-5, 5, ChunkStatus.FULL, true);
-        overworld().getChunk(5, -5, ChunkStatus.FULL, true);
-        overworld().getChunk(5, 5, ChunkStatus.FULL, true);
-        ((ICubicWorld) overworld()).getCube(0, 0, 0, ChunkStatus.FULL, true);
-        ((ICubicWorld) overworld()).getCube(2, 2, 2, ChunkStatus.FULL, true);
-        ((ICubicWorld) overworld()).getCube(-2, 2, 2, ChunkStatus.FULL, true);
-        ((ICubicWorld) overworld()).getCube(2, -2, 2, ChunkStatus.FULL, true);
-        ((ICubicWorld) overworld()).getCube(2, 2, -2, ChunkStatus.FULL, true);
-        ((ICubicWorld) overworld()).getCube(-2, -2, 2, ChunkStatus.FULL, true);
-        ((ICubicWorld) overworld()).getCube(2, -2, -2, ChunkStatus.FULL, true);
-        ((ICubicWorld) overworld()).getCube(-2, 2, -2, ChunkStatus.FULL, true);
-        ((ICubicWorld) overworld()).getCube(-2, -2, -2, ChunkStatus.FULL, true);
-        ((ICubicWorld) overworld()).getCube(5, 5, 5, ChunkStatus.FULL, true);
-        ((ICubicWorld) overworld()).getCube(-5, 5, 5, ChunkStatus.FULL, true);
-        ((ICubicWorld) overworld()).getCube(5, -5, 5, ChunkStatus.FULL, true);
-        ((ICubicWorld) overworld()).getCube(5, 5, -5, ChunkStatus.FULL, true);
-        ((ICubicWorld) overworld()).getCube(-5, -5, 5, ChunkStatus.FULL, true);
-        ((ICubicWorld) overworld()).getCube(5, -5, -5, ChunkStatus.FULL, true);
-        ((ICubicWorld) overworld()).getCube(-5, 5, -5, ChunkStatus.FULL, true);
-        ((ICubicWorld) overworld()).getCube(-5, -5, -5, ChunkStatus.FULL, true);
+        IServerChunkProvider prov = (IServerChunkProvider) serverChunkCache;
+        addChunk(serverChunkCache, new ChunkPos(0, 0));
+        addCube(prov, CubePos.of(0, 0, 0));
+
+        addCube(prov, CubePos.of(5, 5, 5));
+        addCube(prov, CubePos.of(-5, 5, 5));
+        addCube(prov, CubePos.of(-5, -5, 5));
+        addCube(prov, CubePos.of(-5, -5, -5));
+        addCube(prov, CubePos.of(5, -5, 5));
+        addCube(prov, CubePos.of(5, -5, -5));
+        addCube(prov, CubePos.of(5, 5, -5));
+        addCube(prov, CubePos.of(-5, 5, -5));
+
+        while (this.isRunning() && (serverChunkCache.getTickingGenerated() + prov.getTickingGeneratedCubes() < count)) {
+            this.nextTickTime = Util.getMillis() + 10L;
+            this.waitUntilNextTick();
+        }
 
         try {
             Thread.sleep(10000);
