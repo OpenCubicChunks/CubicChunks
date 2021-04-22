80f3b9911d56644b8e5bad338664f4170e48c51f
Corgi Taco <66983020+CorgiTaco@users.noreply.github.com>
2021-04-21 18:08:42 -0700

    -Make Lava Aquifers generate infinitely

--- src/main/java/io/github/opencubicchunks/cubicchunks/mixin/core/common/chunk/cave/MixinAquifer.java
+++ src/main/java/io/github/opencubicchunks/cubicchunks/mixin/core/common/chunk/cave/MixinAquifer.java
@@ -5,7 +5,9 @@ import net.minecraft.world.level.levelgen.Aquifer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(Aquifer.NoiseBasedAquifer.class)
@@ -15,6 +17,11 @@ public abstract class MixinAquifer {
     @Inject(method = "isLavaLevel", at = @At("HEAD"), cancellable = true)
     private void isLavaLevel(int y, CallbackInfoReturnable<Boolean> cir) {
         cir.setReturnValue(y <= CubicChunks.MIN_SUPPORTED_HEIGHT + 12);
+    }
+
+    @ModifyConstant(method = "computeAquifer", constant = @Constant(intValue = -20))
+    private int alwaysTrue(int arg0, int x, int y, int z) {
+        return Math.floorDiv(y, 40) * 40 + 20;
     }
 
     /**
