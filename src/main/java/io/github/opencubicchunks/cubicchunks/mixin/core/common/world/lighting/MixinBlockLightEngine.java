8731263c891ba44a65ca8e7296dc7133ab26e49a
Corgi Taco <66983020+CorgiTaco@users.noreply.github.com>
2021-05-12 14:09:47 -0700

    -Pass checkstyle

--- src/main/java/io/github/opencubicchunks/cubicchunks/mixin/core/common/world/lighting/MixinBlockLightEngine.java
+++ src/main/java/io/github/opencubicchunks/cubicchunks/mixin/core/common/world/lighting/MixinBlockLightEngine.java
@@ -6,7 +6,6 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.core.SectionPos;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.lighting.BlockLightEngine;
-import net.minecraft.world.level.lighting.BlockLightSectionStorage;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
