53847757c7998f3541fccc8c9b5d23dd23e4077d
Niko <niko.sk2x@gmail.com>
2025-11-11 02:30:45 +0100

    🤦

--- build.gradle.kts
+++ build.gradle.kts
@@ -414,16 +414,9 @@ publishing {
 
             setUrl(if (local) localUrl else if (doRelease.toBoolean()) releasesRepoUrl else snapshotsRepoUrl)
             if (!local) {
-                if (doRelease.toBoolean()) {
-                    credentials {
-                        username = ossrhMavenUsername
-                        password = ossrhMavenPassword
-                    }
-                } else {
-                    credentials(HttpHeaderCredentials::class)
-                    authentication {
-                        create<HttpHeaderAuthentication>("header")
-                    }
+                credentials {
+                    username = ossrhMavenUsername
+                    password = ossrhMavenPassword
                 }
             }
         }
